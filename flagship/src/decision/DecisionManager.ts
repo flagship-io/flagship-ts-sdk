70a73da1c25bde9c466175465193593286ea1998
guigithub <guillaume.jacquart@abtasty.com>
2021-05-28 16:37:10 +0200

    Async getCampaigns

--- flagship/src/decision/DecisionManager.ts
+++ flagship/src/decision/DecisionManager.ts
@@ -28,7 +28,7 @@ export abstract class DecisionManager implements IDecisionManager {
   abstract getCampaigns(
     visitorId: string,
     context: Map<string, Object>
-  ): Array<Campaign>;
+  ): Promise<Array<Campaign>>;
 
   public isPanic(): boolean {
     return this._panic;
