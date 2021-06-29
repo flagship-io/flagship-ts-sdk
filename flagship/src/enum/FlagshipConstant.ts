4b22b6f20dde7955e358a2e365a98693e1f85384
Merveille KITOKO <merkitoko@gmail.com>
2021-06-29 16:45:24 +0100

    refactor constants string format

--- flagship/src/enum/FlagshipConstant.ts
+++ flagship/src/enum/FlagshipConstant.ts
@@ -35,16 +35,15 @@ export const ERROR = "error";
 export const CONTEXT_PARAM_ERROR =
   "params 'key' must be a non null String, and 'value' must be one of the following types , Number, Boolean";
 export const GET_MODIFICATION_CAST_ERROR =
-  "Modification for key '%s' has a different type. Default value is returned.";
+  "Modification for key {0} has a different type. Default value is returned.";
 export const GET_MODIFICATION_MISSING_ERROR =
-  "No modification for key '%s'. Default value is returned.";
+  "No modification for key {0}. Default value is returned.";
 export const GET_MODIFICATION_KEY_ERROR =
-  "Key '%s' must not be null. Default value is returned.";
+  "Key {0} must not be null. Default value is returned.";
 export const GET_MODIFICATION_ERROR = "No modification for key '%s'.";
 export const DECISION_MANAGER_MISSING_ERROR =
   "decisionManager must not be null.";
-export const TRACKER_MANAGER_MISSING_ERROR =
-  "trackerManager must not be null.";
+export const TRACKER_MANAGER_MISSING_ERROR = "trackerManager must not be null.";
 export const CURL_LIBRARY_IS_NOT_LOADED = "curl library is not loaded";
 export const TYPE_ERROR = " '%s' must be a '%s'";
 export const VISITOR_ID_ERROR = "visitorId must not be null or empty";
@@ -57,6 +56,5 @@ export const PROCESS_GET_MODIFICATION = "GET MODIFICATION";
 export const PROCESS_GET_MODIFICATION_INFO = "GET MODIFICATION INFO";
 export const PROCESS_NEW_VISITOR = "NEW VISITOR";
 export const PROCESS_ACTIVE_MODIFICATION = "ACTIVE MODIFICATION";
-export const PROCESS_SYNCHRONIZED_MODIFICATION =
-  "SYNCHRONIZED MODIFICATION";
+export const PROCESS_SYNCHRONIZED_MODIFICATION = "SYNCHRONIZED MODIFICATION";
 export const PROCESS_SEND_HIT = "SEND HIT";
