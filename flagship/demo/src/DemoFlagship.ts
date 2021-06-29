def66847c1ef14e05bf4e206caef695deb223871
Merveille KITOKO <merkitoko@gmail.com>
2021-06-29 16:51:06 +0100

    working [ getModification method ]

--- flagship/demo/src/DemoFlagship.ts
+++ flagship/demo/src/DemoFlagship.ts
@@ -2,23 +2,19 @@ import { DecisionApiConfig } from "../../src/config/DecisionApiConfig.ts";
 import { Flagship } from "../../src/main/Flagship.ts";
 
 Flagship.start(
-    "c0n48jn5thv01k0ijmo0",
-    "BsIK86oh7c12c9G7ce4Wm1yBlWeaMf3t1S0xyYzI",
-    new DecisionApiConfig()
+  "c0n48jn5thv01k0ijmo0",
+  "BsIK86oh7c12c9G7ce4Wm1yBlWeaMf3t1S0xyYzI",
+  new DecisionApiConfig()
 );
 
 (async () => {
-const context = new Map<string, string | boolean | number>();
-context.set("age",25);
+  const context = new Map<string, string | boolean | number>();
+  context.set("age", 25);
 
-const visitor = Flagship.newVisitor(
-    "toto",
-    context
-);
+  const visitor = Flagship.newVisitor("toto", context);
 
-if (visitor) {
+  if (visitor) {
     await visitor.synchronizeModifications();
-    console.log(visitor);
-}
-})()
-
+    console.log(visitor.getModification("IsVIP", false));
+  }
+})();
--- flagship/src/visitor/Visitor.ts
+++ flagship/src/visitor/Visitor.ts
@@ -1,58 +1,141 @@
-import {Modification} from "../model/Modification.ts";
-import {ConfigManager} from "../config/ConfigManager.ts";
+import { Modification } from "../model/Modification.ts";
+import { ConfigManager } from "../config/ConfigManager.ts";
+import {
+  VISITOR_ID_ERROR,
+  GET_MODIFICATION_KEY_ERROR,
+  GET_MODIFICATION_MISSING_ERROR,
+  GET_MODIFICATION_CAST_ERROR,
+} from "../enum/FlagshipConstant.ts";
+import { sprintf } from "../utils/utils.ts";
 
 export class Visitor {
-    private _visitorId: string;
-    private _context!: Map<string,string|number|boolean>;
-    private _modifications: Map<string, Modification>;
-    private _configManager: ConfigManager;
+  private _visitorId: string;
+  private _context!: Map<string, string | number | boolean>;
+  private _modifications: Map<string, Modification>;
+  private _configManager: ConfigManager;
 
-    constructor(
-        visitorId: string,
-        context: Map<string, string | number | boolean>,
-        configManager: ConfigManager
+  constructor(
+    visitorId: string,
+    context: Map<string, string | number | boolean>,
+    configManager: ConfigManager
+  ) {
+    this._visitorId = visitorId;
+    this._modifications = new Map<string, Modification>();
+    this._configManager = configManager;
+    this._context = new Map<string, string | number | boolean>();
+    this.updateContext(context);
+  }
+
+  public get visitorId(): string {
+    return this._visitorId;
+  }
+
+  public set visitorId(v: string) {
+    //To do config.Logger
+    if (!v) {
+      console.log(VISITOR_ID_ERROR);
+      return;
+    }
+    this._visitorId = v;
+  }
+
+  public get context(): Map<string, string | number | boolean> {
+    return this._context;
+  }
+
+  /**
+   * Clear the current context and set a new context value
+   */
+  public set context(v: Map<string, string | number | boolean>) {
+    this._context.clear();
+    this.updateContext(v);
+  }
+
+  public get modifications(): Map<string, Modification> {
+    return this._modifications;
+  }
+
+  get configManager(): ConfigManager {
+    return this._configManager;
+  }
+
+  public updateContext(context: Map<string, string | number | boolean>): void {
+    if (!context) {
+      console.log("contest is null");
+      return;
+    }
+    for (const [k, v] of context) {
+      this.updateContextKeyValue(k, v);
+    }
+  }
+
+  public updateContextKeyValue(
+    key: string,
+    value: string | number | boolean
+  ): void {
+    const valueType = typeof value;
+    if (
+      typeof key != "string" ||
+      key == "" ||
+      (valueType != "string" && valueType != "number" && valueType != "boolean")
     ) {
-        this._visitorId = visitorId;
-        this._modifications = new Map<string, Modification>();
-        this._configManager = configManager;
-        this.updateContext(context);
+      //To Do change to config.logManager
+      console.log(
+        `params 'key' must be a non null String, and 'value' must be one of the following types : String, Number, Boolean`
+      );
+      return;
+    }
+    this._context.set(key, value);
+  }
+
+  /**
+   * clear the actual visitor context
+   */
+  public clearContext(): void {
+    this._context.clear();
+  }
+
+  /**
+   * Retrieve a modification value by its key. If no modification match the given
+   * key or if the stored value type and default value type do not match, default value will be returned.
+   *
+   */
+  public getModification<T>(key: string, defaultValue: T, activate = false): T {
+    if (!key) {
+      console.log(sprintf(GET_MODIFICATION_KEY_ERROR, key));
+      return defaultValue;
     }
 
-
-    get configManager(): ConfigManager {
-        return this._configManager;
+    const modification = this._modifications.get(key);
+    if (!modification) {
+      console.log(sprintf(GET_MODIFICATION_MISSING_ERROR, key));
+      return defaultValue;
     }
 
-    public updateContext(context: Map<string, string | number | boolean>): void {
-        if (context != null) {
-            for (const [k, v] of Object.entries(context)) {
-                this.updateContextKeyValue(k, v);
-            }
-        }
+    if (typeof modification.getValue() !== typeof defaultValue) {
+      console.log(sprintf(GET_MODIFICATION_CAST_ERROR, key));
+      //To do send activate if true
+      return defaultValue;
     }
 
-    public updateContextKeyValue(key: string, value: string | number | boolean): void {
-        const valueType = typeof (value);
-        if (typeof (key) != "string" || key == "" || (valueType != "string" && valueType != "number" && valueType != "boolean")) {
-            //To Do change to config.logManager
-            console.log(`params 'key' must be a non null String, and 'value' must be one of the following types : String, Number, Boolean`);
-            return
-        }
-        this._context.set(key, value);
-
+    if (activate) {
+      //To do send activate
     }
 
-    public async synchronizeModifications(): Promise<Visitor> {
-        try {
-            const modifications: Map<string, Modification> | undefined =
-                await this.configManager.decisionManager?.getCampaignsModifications(
-                    this._visitorId,
-                    this._context
-                );
-            this._modifications = modifications;
-        } catch (error) {
-            console.log(error);
-        }
-        return this;
+    return modification.getValue();
+  }
+
+  public async synchronizeModifications(): Promise<Visitor> {
+    try {
+      const modifications =
+        await this.configManager.decisionManager?.getCampaignsModifications(
+          this._visitorId,
+          this._context
+        );
+      this._modifications = modifications;
+    } catch (error) {
+      console.log(error);
     }
+    return this;
+  }
 }
