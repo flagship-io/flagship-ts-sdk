f3a6f8d9834546890a55b71bc3243043f5fd25fa
Merveille KITOKO <merkitoko@gmail.com>
2021-07-01 20:11:49 +0100

    refactor ApiManager

--- flagship/src/decision/ApiManager.ts
+++ flagship/src/decision/ApiManager.ts
@@ -1,55 +1,81 @@
-import { BASE_API_URL, URL_CAMPAIGNS } from "../enum/FlagshipConstant.ts";
+import {
+  BASE_API_URL,
+  EXPOSE_ALL_KEYS,
+  HEADER_APPLICATION_JSON,
+  HEADER_CONTENT_TYPE,
+  HEADER_X_API_KEY,
+  HEADER_X_SDK_CLIENT,
+  HEADER_X_SDK_VERSION,
+  SDK_LANGUAGE,
+  SDK_VERSION,
+  URL_CAMPAIGNS,
+} from "../enum/FlagshipConstant.ts";
 import { DecisionManager } from "./DecisionManager.ts";
 import { CampaignDTO } from "./api/models.ts";
 import { Modification } from "../model/Modification.ts";
+import { Visitor } from "../visitor/Visitor.ts";
+import { logError } from "../utils/utils.ts";
 
 export class ApiManager extends DecisionManager {
-
-  async getCampaignsModifications(
-    visitorId: string,
-    context: Map<string, string | number | boolean>
-  ): Promise<Map<string, Modification>> {
-    const modifMap: Map<string, Modification> = new Map<string, Modification>();
+  private async getCampaignsAsync(visitor: Visitor) {
     try {
-      const data = await (
-        await fetch(BASE_API_URL + this._config.envId + URL_CAMPAIGNS, {
-          method: "POST",
-          headers: {
-            "x-api-key": `${this._config.apiKey}`,
-            "x-sdk-client": "Typescript",
-            "x-sdk-version": "2.0.0",
-          },
-          body: JSON.stringify({
-            visitorId: visitorId,
-            trigger_hit: false,
-            context: context,
-          }),
-        })
-      ).json();
+      const headers = {
+        [HEADER_X_API_KEY]: `${this.config.apiKey}`,
+        [HEADER_X_SDK_CLIENT]: SDK_LANGUAGE,
+        [HEADER_X_SDK_VERSION]: SDK_VERSION,
+        [HEADER_CONTENT_TYPE]: HEADER_APPLICATION_JSON,
+      };
 
-      if (data != null) {
-        const newCampaigns: Array<CampaignDTO> = data.campaigns;
-        newCampaigns.forEach((campaign) => {
-          Object.entries(campaign.variation.modifications.value).forEach(
-            ([k, v]) => {
-              modifMap.set(
-                k,
-                new Modification(
-                  k,
-                  campaign.id,
-                  campaign.variationGroupId,
-                  campaign.variation.id,
-                  campaign.variation.reference,
-                  v
-                )
-              );
-            }
-          );
-        });
+      const postData = {
+        visitorId: visitor.visitorId,
+        // deno-lint-ignore camelcase
+        trigger_hit: false,
+        context: Object.fromEntries(visitor.context),
+      };
+      const url = `${BASE_API_URL}${this.config.envId}${URL_CAMPAIGNS}?${EXPOSE_ALL_KEYS}=true`;
+      const data = await this._httpClient.postAsync(url, {
+        headers,
+        timeout: this.config.timeout,
+        body: postData,
+      });
+      this.panic = false;
+      if (data.body.panic) {
+        this.panic = true;
       }
-    } catch (e) {
-      console.log("Error when calling Decision API: ", e);
+      return data.body.campaigns;
+    } catch (error) {
+      logError(this.config, error.message, "sendActive");
     }
-    return modifMap;
+  }
+
+  private getModifications(campaigns: Array<CampaignDTO>) {
+    const modifications = new Map<string, Modification>();
+    campaigns.forEach((campaign) => {
+      console.log("campaign", campaign);
+
+      Object.entries(campaign.variation.modifications.value).forEach(
+        ([key, value]) => {
+          modifications.set(
+            key,
+            new Modification(
+              key,
+              campaign.id,
+              campaign.variationGroupId,
+              campaign.variation.id,
+              campaign.variation.reference,
+              value
+            )
+          );
+        }
+      );
+    });
+    return modifications;
+  }
+
+  public async getCampaignsModificationsAsync(
+    visitor: Visitor
+  ): Promise<Map<string, Modification>> {
+    const campaigns = await this.getCampaignsAsync(visitor);
+    return this.getModifications(campaigns);
   }
 }
