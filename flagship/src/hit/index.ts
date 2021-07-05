de767a94be9b188dc7038fd2c6d12cf8c3376b78
Merveille KITOKO <merkitoko@gmail.com>
2021-07-05 12:16:43 +0100

    add HitAbstact in hit index file

--- flagship/src/hit/index.ts
+++ flagship/src/hit/index.ts
@@ -3,3 +3,4 @@ export { Item } from "./Item.ts";
 export { Page } from "./Page.ts";
 export { Screen } from "./Screen.ts";
 export { Transaction } from "./Transaction.ts";
+export { HitAbstract } from "./HitAbstract.ts";
