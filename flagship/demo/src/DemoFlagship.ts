07b8d0f5b5de0d31dd737daa86f7ff43e9912ddd
Merveille KITOKO <merkitoko@gmail.com>
2021-07-05 12:16:58 +0100

    add mod file

--- flagship/demo/src/DemoFlagship.ts
+++ flagship/demo/src/DemoFlagship.ts
@@ -1,41 +1,44 @@
-import { DecisionApiConfig } from "../../src/config/DecisionApiConfig.ts";
-import { Flagship } from "../../src/main/Flagship.ts";
-import { API_KEY, ENV_ID } from "./env.ts";
 import {
+  DecisionApiConfig,
+  Flagship,
   EventCategory,
   Item,
   Event,
   Page,
   Screen,
   Transaction,
-} from "../../src/hit/index.ts";
+  FlagshipStatus,
+} from "../../mod.ts";
+import { API_KEY, ENV_ID } from "./env.ts";
 
-Flagship.start(ENV_ID, API_KEY, new DecisionApiConfig());
+const config = new DecisionApiConfig();
+config.statusChangedCallback = (status) => {
+  console.log("status", FlagshipStatus[status]);
+};
 
-(async () => {
-  let count = 0;
-  while (true) {
-    const visitor = Flagship.newVisitor(`visitor_${count}`);
+Flagship.start(ENV_ID, API_KEY, config);
 
-    if (visitor) {
-      await visitor.synchronizeModifications();
-      console.log(visitor.getModification("object", {}));
-      visitor.activateModification("object");
+const visitor = Flagship.newVisitor(`visitor_1`);
 
-      const event = new Event(EventCategory.ACTION_TRACKING, "click");
-      visitor.sendHit(event);
-      const item = new Item("transaction_1", "product_name", "00255578");
-      visitor.sendHit(item);
-      const page = new Page("home");
-      visitor.sendHit(page);
-      const screen = new Screen("home");
-      visitor.sendHit(screen);
-      const transaction = new Transaction(
-        `transaction${count.toString().substr(0, 1)}`,
-        "affiliation"
-      );
-      visitor.sendHit(transaction);
-      console.log(count++);
-    }
-  }
-})();
+if (visitor) {
+  await visitor.synchronizeModifications();
+  console.log(visitor.getModification("object", {}));
+  visitor.activateModification("object");
+
+  console.log(visitor.getModificationInfo("object"));
+
+  const event = new Event(EventCategory.ACTION_TRACKING, "click");
+  visitor.sendHit(event);
+
+  const item = new Item("transaction_1", "product_name", "00255578");
+  visitor.sendHit(item);
+
+  const page = new Page("home");
+  visitor.sendHit(page);
+
+  const screen = new Screen("home");
+  visitor.sendHit(screen);
+
+  const transaction = new Transaction(`transaction_1`, "affiliation");
+  visitor.sendHit(transaction);
+}
