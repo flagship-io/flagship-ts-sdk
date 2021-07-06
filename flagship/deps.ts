6a9f8f0b644d6d574e5cb1b4276964d824db77d1
Merveille KITOKO <merkitoko@gmail.com>
2021-07-06 13:48:26 +0100

    add mock dependency

--- flagship/deps.ts
+++ flagship/deps.ts
@@ -1,1 +1,3 @@
 export * from "https://deno.land/std@0.100.0/testing/asserts.ts";
+export type { Stub } from "https://deno.land/x/mock@v0.9.5/stub.ts";
+export { stub } from "https://deno.land/x/mock@v0.9.5/stub.ts";
