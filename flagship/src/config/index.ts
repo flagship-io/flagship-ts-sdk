172f99cb1cdd9b5fc6e5b9453cf3f710fcab2b43
Merveille KITOKO <merkitoko@gmail.com>
2021-07-06 15:08:57 +0100

    add DecisionMode in config index

--- flagship/src/config/index.ts
+++ flagship/src/config/index.ts
@@ -1,4 +1,4 @@
 export * from "./ConfigManager.ts";
 export * from "./DecisionApiConfig.ts";
-export { FlagshipConfig } from "./FlagshipConfig.ts";
+export { FlagshipConfig, DecisionMode } from "./FlagshipConfig.ts";
 export type { IFlagshipConfig } from "./FlagshipConfig.ts";
