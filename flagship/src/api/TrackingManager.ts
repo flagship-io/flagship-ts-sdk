b8f192c6b462ad52bb9dd0f60346ec77cfe4fc57
Merveille KITOKO <merkitoko@gmail.com>
2021-07-06 13:49:07 +0100

    refactor TrackingManager

--- flagship/src/api/TrackingManager.ts
+++ flagship/src/api/TrackingManager.ts
@@ -24,57 +24,49 @@ import { TrackingManagerAbstract } from "./TrackingManagerAbstract.ts";
 
 export class TrackingManager extends TrackingManagerAbstract {
   public sendActive(visitor: Visitor, modification: Modification): void {
-    try {
-      const headers = {
-        [HEADER_X_API_KEY]: `${this.config.apiKey}`,
-        [HEADER_X_SDK_CLIENT]: SDK_LANGUAGE,
-        [HEADER_X_SDK_VERSION]: SDK_VERSION,
-        [HEADER_CONTENT_TYPE]: HEADER_APPLICATION_JSON,
-      };
+    const headers = {
+      [HEADER_X_API_KEY]: `${this.config.apiKey}`,
+      [HEADER_X_SDK_CLIENT]: SDK_LANGUAGE,
+      [HEADER_X_SDK_VERSION]: SDK_VERSION,
+      [HEADER_CONTENT_TYPE]: HEADER_APPLICATION_JSON,
+    };
 
-      const url = `${BASE_API_URL}${URL_ACTIVATE_MODIFICATION}`;
+    const url = `${BASE_API_URL}${URL_ACTIVATE_MODIFICATION}`;
 
-      const postData = {
-        [VISITOR_ID_API_ITEM]: visitor.visitorId,
-        [VARIATION_ID_API_ITEM]: modification.variationId,
-        [VARIATION_GROUP_ID_API_ITEM]: modification.variationGroupId,
-        [CUSTOMER_ENV_ID_API_ITEM]: this.config.envId,
-      };
+    const postData = {
+      [VISITOR_ID_API_ITEM]: visitor.visitorId,
+      [VARIATION_ID_API_ITEM]: modification.variationId,
+      [VARIATION_GROUP_ID_API_ITEM]: modification.variationGroupId,
+      [CUSTOMER_ENV_ID_API_ITEM]: this.config.envId,
+    };
 
-      this.httpClient
-        .postAsync(url, {
-          headers: headers,
-          timeout: this.config.timeout,
-          body: postData,
-        })
-        .catch((error) => {
-          logError(this.config, JSON.stringify(error), PROCESS_SEND_HIT);
-        });
-    } catch (error) {
-      logError(this.config, JSON.stringify(error), PROCESS_SEND_ACTIVATE);
-    }
+    this.httpClient
+      .postAsync(url, {
+        headers: headers,
+        timeout: this.config.timeout,
+        body: postData,
+      })
+      .catch((error) => {
+        logError(this.config, JSON.stringify(error), PROCESS_SEND_ACTIVATE);
+      });
   }
 
   public sendHit(hit: HitAbstract): void {
-    try {
-      const headers = {
-        [HEADER_X_API_KEY]: `${this.config.apiKey}`,
-        [HEADER_X_SDK_CLIENT]: SDK_LANGUAGE,
-        [HEADER_X_SDK_VERSION]: SDK_VERSION,
-        [HEADER_CONTENT_TYPE]: HEADER_APPLICATION_JSON,
-      };
+    const headers = {
+      [HEADER_X_API_KEY]: `${this.config.apiKey}`,
+      [HEADER_X_SDK_CLIENT]: SDK_LANGUAGE,
+      [HEADER_X_SDK_VERSION]: SDK_VERSION,
+      [HEADER_CONTENT_TYPE]: HEADER_APPLICATION_JSON,
+    };
 
-      this.httpClient
-        .postAsync(HIT_API_URL, {
-          headers: headers,
-          timeout: this.config.timeout,
-          body: hit.toApiKeys(),
-        })
-        .catch((error) => {
-          logError(this.config, JSON.stringify(error), PROCESS_SEND_HIT);
-        });
-    } catch (error) {
-      logError(this.config, JSON.stringify(error), PROCESS_SEND_HIT);
-    }
+    this.httpClient
+      .postAsync(HIT_API_URL, {
+        headers: headers,
+        timeout: this.config.timeout,
+        body: hit.toApiKeys(),
+      })
+      .catch((error) => {
+        logError(this.config, JSON.stringify(error), PROCESS_SEND_HIT);
+      });
   }
 }
