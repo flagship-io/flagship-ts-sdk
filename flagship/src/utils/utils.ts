a3499cde8b1ff33dac0b976533245fd56251868a
Merveille KITOKO <merkitoko@gmail.com>
2021-07-07 17:37:52 +0100

    refact method sprintf

--- flagship/src/utils/utils.ts
+++ flagship/src/utils/utils.ts
@@ -8,7 +8,7 @@ export function sprintf(format: string, ...value: any[]): string {
   let formatted = format;
   for (let i = 0; i < value.length; i++) {
     const element = value[i];
-    formatted = formatted.replace(`{${i}}`, element);
+    formatted = formatted.replaceAll(`{${i}}`, element);
   }
   return formatted;
 }
