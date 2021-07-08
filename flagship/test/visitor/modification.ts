f3b6a65074ff4613a99046f579840e2f58b3fa92
Merveille KITOKO <merkitoko@gmail.com>
2021-07-08 19:04:17 +0100

    add visitor test

---
+++ flagship/test/visitor/modification.ts
@@ -0,0 +1,92 @@
+import { Modification } from "../../src/model/Modification.ts";
+
+export const returnModification = new Map<string, Modification>([
+  [
+    "keyNull",
+    new Modification(
+      "keyNull",
+      "c2nrh1hjg50l9thhu8bg",
+      "c2nrh1hjg50l9thhu8cg",
+      "c2nrh1hjg50l9thhu8dg",
+      false,
+      null
+    ),
+  ],
+  [
+    "keyString",
+    new Modification(
+      "keyString",
+      "c2nrh1hjg50l9thhu8bg",
+      "c2nrh1hjg50l9thhu8cg",
+      "c2nrh1hjg50l9thhu8dg",
+      false,
+      "value"
+    ),
+  ],
+  [
+    "keyNumber",
+    new Modification(
+      "keyNumber",
+      "c2nrh1hjg50l9thhu8bg",
+      "c2nrh1hjg50l9thhu8cg",
+      "c2nrh1hjg50l9thhu8dg",
+      false,
+      52
+    ),
+  ],
+  [
+    "keyBoolean",
+    new Modification(
+      "keyBoolean",
+      "c2nrh1hjg50l9thhu8bg",
+      "c2nrh1hjg50l9thhu8cg",
+      "c2nrh1hjg50l9thhu8dg",
+      false,
+      true
+    ),
+  ],
+  [
+    "key",
+    new Modification(
+      "key",
+      "c2nrh1hjg50l9thhu8bg",
+      "c2nrh1hjg50l9thhu8cg",
+      "c2nrh1hjg50l9thhu8dg",
+      false,
+      "value"
+    ),
+  ],
+  [
+    "array",
+    new Modification(
+      "array",
+      "c3ev1afkprbg5u3burag",
+      "c3ev1afkprbg5u3burbg",
+      "c3ev1afkprbg5u3burcg",
+      false,
+      [1, 1, 1]
+    ),
+  ],
+  [
+    "complex",
+    new Modification(
+      "complex",
+      "c3ev1afkprbg5u3burag",
+      "c3ev1afkprbg5u3burbg",
+      "c3ev1afkprbg5u3burcg",
+      false,
+      { carray: [] }
+    ),
+  ],
+  [
+    "object",
+    new Modification(
+      "object",
+      "c3ev1afkprbg5u3burag",
+      "c3ev1afkprbg5u3burbg",
+      "c3ev1afkprbg5u3burcg",
+      false,
+      { value: 123456 }
+    ),
+  ],
+]);
