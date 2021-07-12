edb989284636a31ccfa82ecf8611f0739a492105
Merveille KITOKO <merkitoko@gmail.com>
2021-07-12 17:12:09 +0100

    add Modifcation.test

--- flagship/test/model/Modification.test.ts
+++ flagship/test/model/Modification.test.ts
@@ -1,25 +1,27 @@
-import { assertEquals } from "../../deps.ts";
-import { Modification } from "../../src/model/Modification.ts";
+import { Modification } from "../../src/model/Modification";
+import { expect, it, describe, afterAll } from "@jest/globals";
 
-Deno.test("Test model Modification", () => {
-  const key = "key";
-  const campaignId = "campaignId";
-  const variationGroupId = "variationGroupId";
-  const variationId = "variationId";
-  const isReference = true;
-  const value = "value";
-  const modification = new Modification(
-    key,
-    campaignId,
-    variationGroupId,
-    variationId,
-    isReference,
-    value
-  );
-  assertEquals(modification.key, key);
-  assertEquals(modification.campaignId, campaignId);
-  assertEquals(modification.variationGroupId, variationGroupId);
-  assertEquals(modification.variationId, variationId);
-  assertEquals(modification.isReference, isReference);
-  assertEquals(modification.value, value);
+describe("Test model Modification", () => {
+  it("should ", () => {
+    const key = "key";
+    const campaignId = "campaignId";
+    const variationGroupId = "variationGroupId";
+    const variationId = "variationId";
+    const isReference = true;
+    const value = "value";
+    const modification = new Modification(
+      key,
+      campaignId,
+      variationGroupId,
+      variationId,
+      isReference,
+      value
+    );
+    expect(modification.key).toBe(key);
+    expect(modification.campaignId).toBe(campaignId);
+    expect(modification.variationGroupId).toBe(variationGroupId);
+    expect(modification.variationId).toBe(variationId);
+    expect(modification.isReference).toBe(isReference);
+    expect(modification.value).toBe(value);
+  });
 });
