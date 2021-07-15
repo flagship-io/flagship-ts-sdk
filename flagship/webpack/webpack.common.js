0c6927c9af6dda53f8776f029d8787f95f90ac76
Merveille KITOKO <merkitoko@gmail.com>
2021-07-15 13:59:10 +0100

    add babel-loader

--- flagship/webpack/webpack.common.js
+++ flagship/webpack/webpack.common.js
@@ -1,5 +1,6 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
 const isProduction = process.env.NODE_ENV == 'production';
@@ -17,6 +18,16 @@ const config = {
                 test: /\.(ts|tsx)$/i,
                 loader: 'ts-loader',
                 exclude: ['/node_modules/'],
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }],
             },
         ],
     },
