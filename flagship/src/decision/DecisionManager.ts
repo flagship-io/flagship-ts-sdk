df1676085f7f9088476f4cebd077883c86728ea7
Merveille KITOKO <merkitoko@gmail.com>
2021-07-23 14:03:29 +0100

    Refactor DecisionManager

--- flagship/src/decision/DecisionManager.ts
+++ flagship/src/decision/DecisionManager.ts
@@ -3,6 +3,7 @@ import { Modification } from '../model/Modification'
 import { IFlagshipConfig } from '../config/FlagshipConfig'
 import { IHttpClient } from '../utils/httpClient'
 import { Visitor } from '../visitor/Visitor'
+import { CampaignDTO } from './api/models'
 
 export abstract class DecisionManager implements IDecisionManager {
   protected _config: IFlagshipConfig;
@@ -22,6 +23,10 @@ export abstract class DecisionManager implements IDecisionManager {
     this._config = config
     this._httpClient = httpClient
   }
+
+  abstract getModifications (campaigns: CampaignDTO[]): Map<string, Modification>
+
+  abstract getCampaignsAsync(visitor: Visitor): Promise<CampaignDTO[]>
 
   abstract getCampaignsModificationsAsync(
     visitor: Visitor
