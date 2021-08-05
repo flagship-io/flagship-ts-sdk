f9cb7bd336df671e290cee674928f6bfef3ad943
Merveille KITOKO <merkitoko@gmail.com>
2021-08-05 13:06:02 +0100

    add PanicStrategy

---
+++ flagship/src/visitor/PanicStrategy.ts
@@ -0,0 +1,48 @@
+import { Modification } from '../index'
+import { FlagshipStatus, METHOD_DEACTIVATED_ERROR } from '../enum/index'
+import { modificationsRequested, primitive } from '../types'
+import { logError, sprintf } from '../utils/utils'
+import { DefaultStrategy } from './DefaultStrategy'
+import { HitAbstract, IEvent, IItem, IPage, IScreen, ITransaction } from '../hit/index'
+
+export class PanicStrategy extends DefaultStrategy {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  updateContext (context: Record<string, primitive>): void {
+    this.log('updateContext')
+  }
+
+  clearContext (): void {
+    this.log('clearContext')
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  getModificationSync<T> (params: modificationsRequested<T> | modificationsRequested<T>[], activateAll?: boolean): T | T[] {
+    this.log('getModification')
+    if (Array.isArray(params)) {
+      return params.map(item => {
+        return item.defaultValue
+      })
+    }
+    return params.defaultValue
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public getModificationInfoSync (key: string): Modification | null {
+    this.log('getModificationInfo')
+    return null
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public activateModificationSync (params: string | string[] | { key: string }[]): void {
+    this.log('activateModification')
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public sendHitSync (hit: HitAbstract | HitAbstract[] | IPage | IScreen | IEvent | IItem | ITransaction | (IPage | IScreen | IEvent | IItem | ITransaction)[]): void {
+    this.log('sendHit')
+  }
+
+  private log (methodName:string) {
+    logError(this.config, sprintf(METHOD_DEACTIVATED_ERROR, methodName, FlagshipStatus[FlagshipStatus.READY_PANIC_ON]), methodName)
+  }
+}
---
+++ flagship/test/visitor/PanicStrategy.test.ts
@@ -0,0 +1,87 @@
+import { jest, expect, it, describe } from '@jest/globals'
+import { DecisionApiConfig } from '../../src'
+import { TrackingManager } from '../../src/api/TrackingManager'
+import { ConfigManager } from '../../src/config'
+import { DecisionManager } from '../../src/decision/DecisionManager'
+import { FlagshipStatus, HitType, METHOD_DEACTIVATED_ERROR } from '../../src/enum'
+import { FlagshipLogManager } from '../../src/utils/FlagshipLogManager'
+import { sprintf } from '../../src/utils/utils'
+import { VisitorDelegate, PanicStrategy } from '../../src/visitor'
+
+describe('test NotReadyStrategy', () => {
+  const visitorId = 'visitorId'
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const context: any = {
+    isVip: true
+  }
+
+  const logManager = new FlagshipLogManager()
+  const logError = jest.spyOn(logManager, 'error')
+
+  const config = new DecisionApiConfig({ envId: 'envId', apiKey: 'apiKey' })
+  config.logManager = logManager
+
+  const configManager = new ConfigManager(config, {} as DecisionManager, {} as TrackingManager)
+  const visitorDelegate = new VisitorDelegate(visitorId, context, configManager)
+  const panicStrategy = new PanicStrategy(visitorDelegate)
+
+  it('test updateContext', () => {
+    const methodName = 'updateContext'
+    panicStrategy.updateContext({ key: 'value' })
+    expect(logError).toBeCalledTimes(1)
+    expect(logError).toBeCalledWith(sprintf(METHOD_DEACTIVATED_ERROR, methodName, FlagshipStatus[FlagshipStatus.READY_PANIC_ON]), methodName)
+  })
+
+  it('test clearContext', () => {
+    const methodName = 'clearContext'
+    panicStrategy.clearContext()
+    expect(logError).toBeCalledTimes(1)
+    expect(logError).toBeCalledWith(sprintf(METHOD_DEACTIVATED_ERROR, methodName, FlagshipStatus[FlagshipStatus.READY_PANIC_ON]), methodName)
+  })
+
+  it('test getModification', async () => {
+    const defaultValue = 'value'
+    const value = await panicStrategy.getModification({ key: 'key', defaultValue })
+    const methodName = 'getModification'
+    expect(value).toBe(defaultValue)
+    expect(logError).toBeCalledTimes(1)
+    expect(logError).toBeCalledWith(sprintf(METHOD_DEACTIVATED_ERROR, methodName, FlagshipStatus[FlagshipStatus.READY_PANIC_ON]), methodName)
+  })
+
+  it('test getModification array', () => {
+    const defaultValue = 'value'
+    panicStrategy.getModification([{ key: 'key', defaultValue }]).then((value) => {
+      const methodName = 'getModification'
+
+      expect(value).toEqual([defaultValue])
+      expect(logError).toBeCalledTimes(1)
+      expect(logError).toBeCalledWith(sprintf(METHOD_DEACTIVATED_ERROR, methodName, FlagshipStatus[FlagshipStatus.READY_PANIC_ON]), methodName)
+    })
+  })
+
+  it('test getModificationInfo', () => {
+    panicStrategy.getModificationInfo('key').then((modification) => {
+      const methodName = 'getModificationInfo'
+
+      expect(modification).toBeNull()
+      expect(logError).toBeCalledTimes(1)
+      expect(logError).toBeCalledWith(sprintf(METHOD_DEACTIVATED_ERROR, methodName, FlagshipStatus[FlagshipStatus.READY_PANIC_ON]), methodName)
+    })
+  })
+
+  it('test activateModification', () => {
+    panicStrategy.activateModification('key').then(() => {
+      const methodName = 'activateModification'
+      expect(logError).toBeCalledTimes(1)
+      expect(logError).toBeCalledWith(sprintf(METHOD_DEACTIVATED_ERROR, methodName, FlagshipStatus[FlagshipStatus.READY_PANIC_ON]), methodName)
+    })
+  })
+
+  it('test sendHit', () => {
+    panicStrategy.sendHit({ type: HitType.PAGE, documentLocation: 'home' }).then(() => {
+      const methodName = 'sendHit'
+      expect(logError).toBeCalledTimes(1)
+      expect(logError).toBeCalledWith(sprintf(METHOD_DEACTIVATED_ERROR, methodName, FlagshipStatus[FlagshipStatus.READY_PANIC_ON]), methodName)
+    })
+  })
+})
