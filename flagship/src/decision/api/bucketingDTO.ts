ba0c99c6cdf9b746486aa9c1b902415c829f574a
Merveille KITOKO <merkitoko@gmail.com>
2021-08-06 16:24:44 +0100

    add Bucketing DTO

--- flagship/src/decision/api/bucketingDTO.ts
+++ flagship/src/decision/api/bucketingDTO.ts
@@ -1,30 +1,34 @@
 
-export interface bucketingDTO {
-    campaigns: Array<{
+export interface Targetings{
+    operator:string,
+    key: string,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    value: any
+}
+export interface VariationGroupDTO {
+    id:string,
+    targeting:{
+        targetingGroups:Array<{
+            targetings:Array<Targetings>
+        }>
+    },
+    variations:Array<{
+        id: string
+        modifications: {
+            type: string
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            value:any
+        }
+        allocation?: number
+        reference?: boolean
+    }>
+}
+
+export interface BucketingDTO {
+    panic?:boolean
+    campaigns?: Array<{
         id: string
         type: string
-        variationGroups:Array<{
-            id:string,
-            targeting:{
-                targetingGroups:Array<{
-                    targetings:Array<{
-                        operator:string,
-                        key: string,
-                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                        value: any
-                    }>
-                }>
-            },
-            variations:Array<{
-                id: string
-                modifications: {
-                    type: string
-                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                    value:any
-                }
-                allocation: number
-                reference?: boolean
-            }>
-        }>
+        variationGroups:Array<VariationGroupDTO>
     }>
 }
