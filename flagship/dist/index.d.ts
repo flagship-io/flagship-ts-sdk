cf9b6023d2b87113275076eef7779d3e1aca11f9
Merveille KITOKO <merkitoko@gmail.com>
2021-08-24 10:46:40 +0100

    build

--- flagship/dist/index.d.ts
+++ flagship/dist/index.d.ts
@@ -1,23 +1,10 @@
-export { Flagship } from "./main/Flagship";
-export { DecisionApiConfig, DecisionMode } from "./config/index";
-export type { IFlagshipConfig } from "./config/index";
-export {
-  Event,
-  IEvent,
-  EventCategory,
-  IItem,
-  Item,
-  IPage,
-  Page,
-  IScreen,
-  Screen,
-  ITransaction,
-  Transaction,
-  HitAbstract,
-} from "./hit/index";
-export { FlagshipStatus, LogLevel, HitType } from "./enum/index";
-export * from "./enum/FlagshipContext";
-export { Modification } from "./model/Modification";
-export * from "./types";
-export { Visitor } from "./visitor/index";
-export type { IFlagshipLogManager } from "./utils/FlagshipLogManager";
+export { Flagship } from './main/Flagship';
+export { DecisionApiConfig, DecisionMode } from './config/index';
+export type { IFlagshipConfig } from './config/index';
+export { Event, IEvent, EventCategory, IItem, Item, IPage, Page, IScreen, Screen, ITransaction, Transaction, HitAbstract } from './hit/index';
+export { FlagshipStatus, LogLevel, HitType } from './enum/index';
+export * from './enum/FlagshipContext';
+export { Modification } from './model/Modification';
+export * from './types';
+export { Visitor } from './visitor/index';
+export type { IFlagshipLogManager } from './utils/FlagshipLogManager';
--- flagship/dist/visitor/IVisitor.d.ts
+++ flagship/dist/visitor/IVisitor.d.ts
@@ -1,31 +1,31 @@
-import { IFlagshipConfig } from '../config/index';
-import { CampaignDTO } from '../decision/api/models';
-import { HitAbstract, IEvent, IItem, IPage, IScreen, ITransaction } from '../hit/index';
-import { Modification } from '../model/Modification';
-import { modificationsRequested, primitive } from '../types';
+import { IFlagshipConfig } from "../config/index";
+import { CampaignDTO } from "../decision/api/models";
+import { HitAbstract, IEvent, IItem, IPage, IScreen, ITransaction } from "../hit/index";
+import { Modification } from "../model/Modification";
+import { modificationsRequested, primitive } from "../types";
 export interface IVisitor {
     visitorId: string;
     modifications: Map<string, Modification>;
     context: Record<string, primitive>;
     /**
-       * Return True or False if the visitor has consented for protected data usage.
-       * @return bool
-       */
+     * Return True or False if the visitor has consented for protected data usage.
+     * @return bool
+     */
     hasConsented: boolean;
     /**
-      * Set if visitor has consented for protected data usage.
-      * @param {boolean} hasConsented True if the visitor has consented false otherwise.
-      */
+     * Set if visitor has consented for protected data usage.
+     * @param {boolean} hasConsented True if the visitor has consented false otherwise.
+     */
     setConsent(hasConsented: boolean): void;
     config: IFlagshipConfig;
     /**
-   * Update the visitor context values, matching the given keys, used for targeting.
-   *
-   * A new context value associated with this key will be created if there is no previous matching value.
-   *
-   * Context keys must be String, and values types must be one of the following : Number, Boolean, String.
-   * @param {Record<string, primitive>} context : collection of keys, values.
-   */
+     * Update the visitor context values, matching the given keys, used for targeting.
+     *
+     * A new context value associated with this key will be created if there is no previous matching value.
+     *
+     * Context keys must be String, and values types must be one of the following : Number, Boolean, String.
+     * @param {Record<string, primitive>} context : collection of keys, values.
+     */
     updateContext(context: Record<string, primitive>): void;
     /**
      * clear the actual visitor context
@@ -72,22 +72,22 @@ export interface IVisitor {
      */
     activateModification(key: string): Promise<void>;
     /**
-      * Report this user has seen these modifications.
-      * @deprecated use ["key1","key2",...] instead of
-      * @param {Array<{ key: string }>} keys keys which identify the modifications to activate.
-      */
+     * Report this user has seen these modifications.
+     * @deprecated use ["key1","key2",...] instead of
+     * @param {Array<{ key: string }>} keys keys which identify the modifications to activate.
+     */
     activateModification(keys: Array<{
         key: string;
     }>): Promise<void>;
     /**
-      * Report this user has seen these modifications.
-      * @param keys  keys which identify the modifications to activate.
-      */
+     * Report this user has seen these modifications.
+     * @param keys  keys which identify the modifications to activate.
+     */
     activateModification(keys: Array<string>): Promise<void>;
     /**
-   * Report this user has seen this modification.
-   * @param key : key which identify the modification to activate.
-   */
+     * Report this user has seen this modification.
+     * @param key : key which identify the modification to activate.
+     */
     activateModificationSync(key: string): void;
     /**
      * Report this user has seen these modifications.
@@ -103,9 +103,9 @@ export interface IVisitor {
      */
     activateModificationSync(keys: Array<string>): void;
     /**
-   * Send a Hit to Flagship servers for reporting.
-   * @param hit
-   */
+     * Send a Hit to Flagship servers for reporting.
+     * @param hit
+     */
     sendHit(hit: HitAbstract): Promise<void>;
     sendHit(hit: Array<HitAbstract>): Promise<void>;
     sendHit(hit: IPage | IScreen | IEvent | IItem | ITransaction): Promise<void>;
@@ -127,11 +127,11 @@ export interface IVisitor {
         campaigns: CampaignDTO[];
     }>;
     /**
-    * Get data for a specific campaign.
-    * @param campaignId Identifies the campaign whose modifications you want to retrieve.
-    * @param activate
-    * @deprecated
-    */
+     * Get data for a specific campaign.
+     * @param campaignId Identifies the campaign whose modifications you want to retrieve.
+     * @param activate
+     * @deprecated
+     */
     getModificationsForCampaign(campaignId: string, activate: boolean): Promise<{
         visitorId: string;
         campaigns: CampaignDTO[];
