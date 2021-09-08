fd84833a60b053df0dac98d5b39aba174f64494c
Merveille KITOKO <merkitoko@gmail.com>
2021-09-08 05:33:15 +0100

    refactor axios instance

--- flagship/src/nodeDeps.ts
+++ flagship/src/nodeDeps.ts
@@ -1,1 +1,15 @@
+import axios from 'axios'
+
+import { Agent as HttpsAgent } from 'https'
+import { Agent as HttpAgent } from 'http'
+
+export const axiosInstance = axios.create({
+  httpAgent: new HttpAgent({ keepAlive: true }),
+  httpsAgent: new HttpsAgent({ keepAlive: true })
+})
+
+export const defaultAxios = axios
+
+export { AxiosResponse, AxiosError, AxiosRequestConfig } from 'axios'
+
 export { EventEmitter } from 'events'
