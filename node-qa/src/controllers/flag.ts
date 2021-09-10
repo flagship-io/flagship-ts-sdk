15e3d4dcc89f51043dd80c69bc3949924fbd3a4b
Merveille KITOKO <merkitoko@gmail.com>
2021-09-10 02:26:01 +0100

    add flag controller

---
+++ node-qa/src/controllers/flag.ts
@@ -0,0 +1,110 @@
+import { Request, Response, NextFunction, Visitor } from '../deps'
+import { sessionVisitors } from './visitor'
+
+function checkContextKey (key:string, defaultValue:unknown, type:string):Record<string, unknown> {
+  const error: Record<string, unknown> = {}
+
+  console.log(typeof defaultValue)
+
+  const typeErrorMessage = (argValue: unknown, argType: string) =>
+    `value ${argValue} must be ${argType}`
+
+  switch (type) {
+    case 'bool':
+      if (typeof defaultValue !== 'boolean') {
+        error.defaultValue = typeErrorMessage(defaultValue, type)
+      }
+      break
+    case 'int':
+    case 'float':
+    case 'double':
+    case 'long':
+      if (!/^\d+(([.,]{0,1}\d+)|\d*)$/.test(`${defaultValue}`)) {
+        error.defaultValue = typeErrorMessage(defaultValue, type)
+      }
+      break
+    case 'JSONObject':
+      if (
+        typeof defaultValue !== 'object' ||
+        Array.isArray(defaultValue)
+      ) {
+        error.defaultValue = typeErrorMessage(defaultValue, type)
+      }
+      break
+    case 'JSONArray':
+      if (
+        typeof defaultValue !== 'object' ||
+        !Array.isArray(defaultValue)
+      ) {
+        error.defaultValue = typeErrorMessage(defaultValue, type)
+      }
+      break
+    default:
+      break
+  }
+  return error
+}
+
+export const getFlagValidation = (req: Request, res: Response, next: NextFunction):void => {
+  const { flagKey } = req.params
+  const { type, activate, defaultValue } = req.query
+
+  let parseDefaultValue = defaultValue
+  try {
+    parseDefaultValue = defaultValue ? JSON.parse(defaultValue as string) : ''
+  } catch (err) {
+    console.log(err)
+  }
+
+  const error = checkContextKey(flagKey, parseDefaultValue, type as string)
+
+  if (Object.keys(error).length) {
+    res.status(422).json({ error, ok: true })
+    return
+  }
+
+  res.locals.flagValue = {
+    flagKey,
+    defaultValue: parseDefaultValue,
+    activate: JSON.parse(activate as string)
+  }
+  next()
+}
+
+export const getFlag = async (req: Request, res: Response):Promise<void> => {
+  try {
+    const { flagKey, activate, defaultValue } = res.locals.flagValue
+    const visitor = sessionVisitors[req.session.id]
+    const response = {
+      value: {}
+    }
+    if (visitor) {
+      const modification = await visitor.getModification({
+        key: flagKey,
+        defaultValue,
+        activate
+      })
+      response.value = modification
+    }
+    console.log(req.session)
+
+    res.json(response)
+  } catch (error) {
+    console.log('error', error)
+    res.status(500).json(error)
+  }
+}
+
+export const getFlagInfo = async (req: Request, res: Response):Promise<void> => {
+  const visitor: Visitor = sessionVisitors[req.session.id]
+  const { flagKey } = req.params
+  const data = await visitor.getModificationInfo(flagKey)
+  res.json(data)
+}
+
+export const sendActivate = (req: Request, res: Response):void => {
+  const visitor: Visitor = sessionVisitors[req.session.id]
+  const { flagKey } = req.params
+  visitor.activateModification(flagKey)
+  res.json('successful operation')
+}
