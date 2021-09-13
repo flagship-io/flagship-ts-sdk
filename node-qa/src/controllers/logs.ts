87e4c6595d2f3b009d4e85ffdb28f82882aabaaf
Merveille KITOKO <merkitoko@gmail.com>
2021-09-13 09:25:34 +0100

    remove console.log

--- node-qa/src/controllers/logs.ts
+++ node-qa/src/controllers/logs.ts
@@ -2,9 +2,6 @@ import { Request, Response } from '../deps'
 
 export const getLog = (req: Request, res: Response):void => {
   const logs = req.session.logs
-  console.log('getLog', req.session)
-  console.log('id', req.session.id)
-
   res.send(logs)
 }
 
