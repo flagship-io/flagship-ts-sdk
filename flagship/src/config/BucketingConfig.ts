17dcf85a04e53c4e291b48f2c5b4d5bc5f848bee
Merveille KITOKO <merkitoko@gmail.com>
2021-09-16 16:28:59 +0100

    refactor BucketingConfig constructor

--- flagship/src/config/BucketingConfig.ts
+++ flagship/src/config/BucketingConfig.ts
@@ -4,14 +4,10 @@ import { DecisionMode, FlagshipConfig, IFlagshipConfig } from './FlagshipConfig'
 export class BucketingConfig extends FlagshipConfig {
   public constructor (param?: Omit<IFlagshipConfig, 'decisionMode'>) {
     super({ ...param, decisionMode: DecisionMode.BUCKETING })
-    if (param) {
-      const { pollingInterval, onBucketingFail, onBucketingSuccess, onBucketingUpdated } = param
-      this.pollingInterval = pollingInterval ?? REQUEST_TIME_OUT
-      this.onBucketingFail = onBucketingFail
-      this.onBucketingSuccess = onBucketingSuccess
-      this.onBucketingUpdated = onBucketingUpdated
-    } else {
-      this.pollingInterval = REQUEST_TIME_OUT
-    }
+
+    this.pollingInterval = param?.pollingInterval ?? REQUEST_TIME_OUT
+    this.onBucketingFail = param?.onBucketingFail
+    this.onBucketingSuccess = param?.onBucketingSuccess
+    this.onBucketingUpdated = param?.onBucketingUpdated
   }
 }
