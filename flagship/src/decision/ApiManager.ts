1538885c44d15655d33ae9f58630fc58b02d979d
Merveille KITOKO <merkitoko@gmail.com>
2021-09-16 16:41:16 +0100

    refactor getCampaignsAsync in ApiManager

--- flagship/src/decision/ApiManager.ts
+++ flagship/src/decision/ApiManager.ts
@@ -19,47 +19,41 @@ import { VisitorAbstract } from '../visitor/VisitorAbstract'
 
 export class ApiManager extends DecisionManager {
   public async getCampaignsAsync (visitor: VisitorAbstract):Promise<CampaignDTO[]> {
-    return new Promise((resolve, reject) => {
-      const headers = {
-        [HEADER_X_API_KEY]: `${this.config.apiKey}`,
-        [HEADER_X_SDK_CLIENT]: SDK_LANGUAGE,
-        [HEADER_X_SDK_VERSION]: SDK_VERSION,
-        [HEADER_CONTENT_TYPE]: HEADER_APPLICATION_JSON
-      }
+    const headers = {
+      [HEADER_X_API_KEY]: `${this.config.apiKey}`,
+      [HEADER_X_SDK_CLIENT]: SDK_LANGUAGE,
+      [HEADER_X_SDK_VERSION]: SDK_VERSION,
+      [HEADER_CONTENT_TYPE]: HEADER_APPLICATION_JSON
+    }
 
-      const postData = {
-        visitorId: visitor.visitorId,
-        anonymousId: visitor.anonymousId,
-        trigger_hit: false,
-        context: visitor.context
-      }
+    const postData = {
+      visitorId: visitor.visitorId,
+      anonymousId: visitor.anonymousId,
+      trigger_hit: false,
+      context: visitor.context
+    }
 
-      let url = `${BASE_API_URL}${this.config.envId}${URL_CAMPAIGNS}?${EXPOSE_ALL_KEYS}=true`
-      if (!visitor.hasConsented) {
-        url += `&${SEND_CONTEXT_EVENT}=false`
-      }
+    let url = `${BASE_API_URL}${this.config.envId}${URL_CAMPAIGNS}?${EXPOSE_ALL_KEYS}=true`
+    if (!visitor.hasConsented) {
+      url += `&${SEND_CONTEXT_EVENT}=false`
+    }
 
-      this._httpClient.postAsync(url, {
-        headers,
-        timeout: this.config.timeout,
-        body: postData
-      })
-        .then(data => {
-          if (data.body.panic) {
-            this.panic = true
-          } else {
-            this.panic = false
-          }
-          let response:CampaignDTO[] = []
-          if (data.body.campaigns) {
-            response = data.body.campaigns
-          }
-          resolve(response)
-        })
-        .catch(error => {
-          logError(this.config, JSON.stringify(error), PROCESS_GET_CAMPAIGNS)
-          reject(error)
-        })
+    return this._httpClient.postAsync(url, {
+      headers,
+      timeout: this.config.timeout,
+      body: postData
     })
+      .then(data => {
+        this.panic = !!data.body.panic
+        let response:CampaignDTO[] = []
+        if (data.body.campaigns) {
+          response = data.body.campaigns
+        }
+        return response
+      })
+      .catch(error => {
+        logError(this.config, error.message || error, PROCESS_GET_CAMPAIGNS)
+        return []
+      })
   }
 }
