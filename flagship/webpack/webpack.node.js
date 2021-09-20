ee5d522a8d028346fca4006fd3823a5db02b0a20
Merveille KITOKO <merkitoko@gmail.com>
2021-09-20 15:11:08 +0100

    refactor webpack node

--- flagship/webpack/webpack.node.js
+++ flagship/webpack/webpack.node.js
@@ -1,4 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const webpack = require('webpack')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { merge } = require('webpack-merge')
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const nodeExternals = require('webpack-node-externals')
@@ -13,9 +15,14 @@ module.exports = merge(common(), {
       type: 'commonjs2'
     }
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      AbortController: 'abort-controller'
+    })
+  ],
   externals: [
     nodeExternals({
-      allowlist: ['axios', 'follow-redirects']
+      allowlist: ['node-fetch', 'abort-controller', 'follow-redirects']
     })
   ]
 })
