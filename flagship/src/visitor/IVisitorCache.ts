ee70d58c8666f521a703a49fa8060cb02fd9b6c4
Merveille KITOKO <merkitoko@gmail.com>
2021-11-09 23:05:49 +0100

    add IVisitorCache

---
+++ flagship/src/visitor/IVisitorCache.ts
@@ -0,0 +1,6 @@
+import { VisitorLookupCacheDTO, VisitorSaveCacheDTO } from '../models/visitorDTO'
+
+export interface IVisitorCache {
+    cacheVisitor:(visitorId: string, Data: VisitorSaveCacheDTO)=>void
+    lookupVisitor(visitorId: string): VisitorLookupCacheDTO
+}
