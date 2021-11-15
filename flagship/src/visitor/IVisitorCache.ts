749f7da3dd32bc55ff7b6d008ff50401836512ed
Merveille KITOKO <merkitoko@gmail.com>
2021-11-15 12:08:00 +0100

    add flushVisitorCache in IVisitorCache

--- flagship/src/visitor/IVisitorCache.ts
+++ flagship/src/visitor/IVisitorCache.ts
@@ -3,4 +3,5 @@ import { VisitorLookupCacheDTO, VisitorSaveCacheDTO } from '../models/visitorDTO
 export interface IVisitorCache {
     cacheVisitor:(visitorId: string, Data: VisitorSaveCacheDTO)=>void
     lookupVisitor(visitorId: string): VisitorLookupCacheDTO
+    flushVisitor(visitorId: string):void
 }
