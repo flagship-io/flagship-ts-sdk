cddacd958341455f9921b3c4dc427c1756c5957a
Merveille KITOKO <merkitoko@gmail.com>
2021-11-29 23:48:27 +0100

    add type FlagDTO

--- flagship/src/types.ts
+++ flagship/src/types.ts
@@ -12,7 +12,7 @@ export type primitive=string | number | boolean
 export type { HitShape }
 export type IHit = IPage | IScreen | IEvent | IItem | ITransaction
 
-export type Modification= {
+export type FlagDTO= {
   key: string;
   campaignId: string;
   variationGroupId: string;
@@ -21,6 +21,11 @@ export type Modification= {
  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   value: any;
 }
+
+/**
+ * @deprecated use FlagDTO instead
+ */
+export type Modification= FlagDTO
 
 export type NewVisitor={
   /**
@@ -35,7 +40,11 @@ export type NewVisitor={
   hasConsented?:boolean,
 
    initialCampaigns?: CampaignDTO[]
+   /**
+    * @deprecated use initialFlags instead
+    */
    initialModifications?: Map<string, Modification>|Modification[]
+   initialFlags?: Map<string, FlagDTO>|FlagDTO[]
 
    /**
     * If true The newly created visitor instance won't be saved and will simply be returned otherwise
