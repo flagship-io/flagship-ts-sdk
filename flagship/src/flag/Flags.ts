795f4f93346ca637899ebef47e5df12ce66c694d
Merveille KITOKO <merkitoko@gmail.com>
2021-12-07 17:43:01 +0100

    refactor Flag type

--- flagship/src/flag/Flags.ts
+++ flagship/src/flag/Flags.ts
@@ -1,14 +1,21 @@
 import { FlagDTO } from '../types'
+import { hasSameType } from '../utils/utils'
 import { VisitorDelegate } from '../visitor/index'
 import { FlagMetadata, IFlagMetadata } from './FlagMetadata'
 
-export interface IFlag{
+export type FlagValue<S> = {
+  defaultValue: S,
+  userExposed?: boolean
+}
+
+export interface IFlag<T>{
   /**
    * Return the current flag value if the flag key exists in Flagship and activate it if needed.
    * @param defaultValue
    * @param userExposed
    */
-    value<T>(defaultValue:T, userExposed?:boolean):T
+    value():T
+    value<S> (param:FlagValue<S>) : S
     /**
      * Return true if the flag exists, false otherwise.
      */
@@ -23,15 +30,18 @@ export interface IFlag{
     metadata:IFlagMetadata
 }
 
-export class Flag implements IFlag {
+export class Flag<T> implements IFlag<T> {
     private _visitor:VisitorDelegate
     private _flagDTO?: FlagDTO
     private _metadata:IFlagMetadata
     private _key:string
-    constructor (key:string, visitor:VisitorDelegate, flagDTO?:FlagDTO) {
+    private _defaultValue:any
+    constructor (param: {key:string, visitor:VisitorDelegate, flagDTO?:FlagDTO, defaultValue:T}) {
+      const { key, visitor, flagDTO, defaultValue } = param
       this._key = key
       this._flagDTO = flagDTO
       this._visitor = visitor
+      this._defaultValue = defaultValue
       this._metadata = new FlagMetadata({
         campaignId: flagDTO?.campaignId || '',
         scenarioId: '',
@@ -47,14 +57,27 @@ export class Flag implements IFlag {
     }
 
     get metadata ():IFlagMetadata {
-      return this._visitor.getFlagMetadata(this._metadata)
+      return this._visitor.getFlagMetadata({
+        metadata: this._metadata,
+        hasSameType: hasSameType(this._flagDTO?.value, this._defaultValue)
+      })
     }
 
     userExposed ():Promise<void> {
-      return this._visitor.userExposed(this._key, this._flagDTO)
+      return this._visitor.userExposed({ key: this._key, flag: this._flagDTO, defaultValue: this._defaultValue })
     }
 
-    value<T> (defaultValue: T, userExposed = true): T {
-      return this._visitor.getFlagValue({ key: this._key, defaultValue, flag: this._flagDTO, userExposed })
+    value():T
+    value<S> (param:FlagValue<S>) : S
+    value<S> (param?:FlagValue<S>) : S|T {
+      this._defaultValue = param?.defaultValue ?? this._defaultValue
+      const userExposed = param?.userExposed ?? true
+
+      return this._visitor.getFlagValue({
+        key: this._key,
+        defaultValue: this._defaultValue,
+        flag: this._flagDTO,
+        userExposed: userExposed
+      })
     }
 }
