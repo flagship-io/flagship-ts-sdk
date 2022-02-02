641d7cef74cb400990d0fe2e43dd9bc1d5337e3a
Merveille KITOKO <merkitoko@gmail.com>
2022-02-02 10:39:40 +0100

    demo

--- flagship/demo/dist/DemoFlagship.js
+++ flagship/demo/dist/DemoFlagship.js
@@ -11,104 +11,90 @@ var _campaigns = require('./campaigns');
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const sleep = ms => {
-  return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
 };
 
 const statusChangedCallback = status => {
-  console.log('status', _jsSdk.FlagshipStatus[status]);
+    console.log('status', _jsSdk.FlagshipStatus[status]);
 };
+
+const check = {};
 
 _jsSdk2.default.start(_config.ENV_ID, _config.API_KEY, {
-  decisionMode: _jsSdk.DecisionMode.BUCKETING,
-  statusChangedCallback,
-  logLevel: _jsSdk.LogLevel.ERROR,
-  fetchNow: false,
-  timeout: 10
+    // decisionMode: DecisionMode.BUCKETING,
+    statusChangedCallback,
+    logLevel: _jsSdk.LogLevel.ERROR,
+    fetchNow: false,
+    timeout: 10
 });
 
-const initialModifications = new Map([['array', {
-  key: 'array',
-  campaignId: 'c3ev1afkprbg5u3burag',
-  variationGroupId: 'c3ev1afkprbg5u3burbg',
-  variationId: 'c3ev1afkprbg5u3burcg',
-  isReference: false,
-  value: [1, 1, 1]
-}]]);
-
-const visitor = _jsSdk2.default.newVisitor({ visitorId: 'visitor_id', context: { key: 'value' }, initialModifications, initialCampaigns: _campaigns.campaigns.campaigns });
-
-const start = async () => {
-  if (visitor) {
+const start = async (visitor, index) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     visitor.on('ready', async err => {
-      if (err) {
-        console.log('Flagship error:', err);
-        return;
-      }
-      console.log('Flagship Ready');
+        if (err) {
+            console.log('Flagship error:', err);
+            return;
+        }
+        console.log('Flagship Ready');
+
+        // getFlag
+        const flag = visitor.getFlag('js-qa-app', 'test');
+
+        const value = flag.value(false);
+
+        if (check[value]) {
+            check[value] += 1;
+        } else {
+            check[value] = 1;
+        }
+
+        console.log('flag.value', value);
+
+        await flag.userExposed();
+
+        console.log('flag.userExposed');
+        // send hit
+
+        // hit type Event
+        await visitor.sendHit({
+            type: _jsSdk.HitType.EVENT,
+            category: _jsSdk.EventCategory.ACTION_TRACKING,
+            action: 'KPI2',
+            value: 10
+        });
+
+        console.log('hit type Event');
+
+        await flag.userExposed();
+        // hit type Page
+        await visitor.sendHit({ type: _jsSdk.HitType.PAGE, documentLocation: 'https://www.sdk.com/abtastylab/js/151021-' + index });
+        console.log('hit type Page');
+
+        await flag.userExposed();
+        // hit type Screen
+        await visitor.sendHit({ type: _jsSdk.HitType.SCREEN, documentLocation: 'abtastylab-js-' + index });
+
+        console.log('hit type Screen');
+
+        await flag.userExposed();
+        // hit type Transaction
+        const transaction = new _jsSdk.Transaction({ transactionId: visitor.visitorId, affiliation: 'KPI1' });
+        await visitor.sendHit(transaction);
+
+        console.log('hit type transaction');
+
+        console.log('check', check);
     });
 
-    // await sleep(5000)
-    // clear context
-    visitor.clearContext();
-
-    // Update context
-    visitor.updateContext({ isVip: true });
-
-    // optional when fetchNow = true, this method is call on each newVisitor
-    // await visitor.synchronizeModifications()
-    visitor.setConsent(true);
-
-    // getModification
-    visitor.getModification({ key: 'object', defaultValue: {} }).then(modification => {
-      console.log('modification:', modification);
-    });
-
-    visitor.getModifications([{ key: 'array', defaultValue: [] }, {
-      key: 'object',
-      defaultValue: {},
-      activate: true
-    }]).then(modifications => {
-      console.log('modifications:', modifications);
-    });
-
-    // activateModification
-    visitor.activateModification('object');
-
-    visitor.activateModifications(['array', 'object']);
-
-    // getModificationInfo
-    visitor.getModificationInfo('array').then(data => {
-      console.log('info', data);
-    });
-
-    // send hit
-
-    // hit type Event
-    visitor.sendHit({
-      type: _jsSdk.HitType.EVENT,
-      category: _jsSdk.EventCategory.ACTION_TRACKING,
-      action: 'click'
-    });
-
-    // hit type Item
-    const item = new _jsSdk.Item({
-      transactionId: 'transaction_1',
-      productName: 'product_name',
-      productSku: '00255578'
-    });
-
-    visitor.sendHit(item);
-
-    // hit type Page
-    visitor.sendHit({ type: _jsSdk.HitType.PAGE, documentLocation: 'https://localhost' });
-
-    // hit type Screen
-    visitor.sendHit({ type: _jsSdk.HitType.SCREEN, documentLocation: 'https://localhost' });
-
-    // hit type Transaction
-    const transaction = new _jsSdk.Transaction({ transactionId: 'transaction_1', affiliation: 'affiliation' });
-    visitor.sendHit(transaction);
-  }
+    await visitor.fetchFlags();
 };
-start();
+
+async function script() {
+    await sleep(2000);
+    for (let index = 0; index <= 1; index++) {
+        const visitor = _jsSdk2.default.newVisitor({ visitorId: '151022-js-trans-' + index, context: { key: 'value' } });
+        await start(visitor, index);
+    }
+}
+
+script();
