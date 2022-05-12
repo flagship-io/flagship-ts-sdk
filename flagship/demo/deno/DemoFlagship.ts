f1d7902f14ba914dd97290db689c9f65baad6a09
Heihokon <neoshark22@gmail.com>
2022-05-12 13:09:10 +0100

    demo

--- flagship/demo/deno/DemoFlagship.ts
+++ flagship/demo/deno/DemoFlagship.ts
@@ -9,141 +9,93 @@ import {
   Transaction,
   Modification,
   DEVICE_LOCALE
-} from 'https://deno.land/x/flagship_io_js_sdk@3.0.0/mod.ts'
+} from 'https://deno.land/x/flagship_io_js_sdk@3.0.7/mod.ts'
 import { API_KEY, ENV_ID } from './config.js'
+const sleep = (ms:number) => {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
 
-const statusChangedCallback = (status: FlagshipStatus) => {
+const statusChangedCallback = (status:any) => {
   console.log('status', FlagshipStatus[status])
 }
 
-function sleep (ms: number): Promise<unknown> {
-  return new Promise((resolve) => setTimeout(resolve, ms))
-}
+const check:any = {}
 
 Flagship.start(ENV_ID, API_KEY, {
-  decisionMode: DecisionMode.BUCKETING,
+  // decisionMode: DecisionMode.BUCKETING,
   statusChangedCallback,
-  logLevel: LogLevel.ALL,
+  logLevel: LogLevel.ERROR,
   fetchNow: false,
-  timeout: 5
+  timeout: 10
 })
 
-const visitor = Flagship.newVisitor({
-  visitorId: 'visitor_id',
-  isAuthenticated: true,
-  context: {
-    key: 'value',
-    [DEVICE_LOCALE]: 'fr'
-  }
-});
-
-(async () => {
-  if (visitor) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    // deno-lint-ignore no-explicit-any
-    visitor.on('ready', (err: any) => {
-      if (err) {
-        console.log('Flagship error:', err)
-        return
-      }
-      console.log('Flagship Ready')
-    })
-    console.log(visitor)
-    // clear context
-    visitor.clearContext()
-
-    // Update context
-    visitor.updateContext({ isOP: true })
-
-    visitor.setConsent(true)
-
-    await sleep(5000)
-
-    await visitor.fetchFlags()
-
-    const modification = await visitor.getModification({
-      key: 'object',
-      defaultValue: {}
-    })
-    console.log('modification:', modification)
-
-    // Flagship.start(ENV_ID, API_KEY, {
-    //   decisionMode: DecisionMode.DECISION_API,
-    //   statusChangedCallback,
-    //   logLevel: LogLevel.ALL,
-    //   fetchNow: false,
-    // });
-
-    for (let index = 0; index < 5; index++) {
-      // optional when fetchNow = true, this method is call on each newVisitor
-      await visitor.synchronizeModifications()
-
-      // getModification
-      visitor
-        .getModification({ key: 'object', defaultValue: {} })
-        .then((modification) => {
-          console.log('modification:', modification)
-        })
-
-      visitor
-        .getModifications([
-          { key: 'array', defaultValue: [] },
-          {
-            key: 'object',
-            defaultValue: {},
-            activate: true
-          }
-        ])
-        .then((modifications) => {
-          console.log('modifications:', modifications)
-        })
-
-      // activateModification
-      visitor.activateModification('object')
-
-      visitor.activateModifications(['array', 'object'])
-
-      // getModificationInfo
-      visitor.getModificationInfo('array').then((data: Modification | null) => {
-        console.log('info', data)
-      })
-
-      // send hit
-
-      // hit type Event
-      visitor.sendHit({
-        type: HitType.EVENT,
-        category: EventCategory.ACTION_TRACKING,
-        action: 'click'
-      })
-
-      // hit type Item
-      const item = new Item({
-        transactionId: 'transaction_1',
-        productName: 'product_name',
-        productSku: '00255578'
-      })
-
-      visitor.sendHit(item)
-
-      // hit type Page
-      visitor.sendHit({
-        type: HitType.PAGE,
-        documentLocation: 'https://localhost'
-      })
-
-      // hit type Screen
-      visitor.sendHit({
-        type: HitType.SCREEN,
-        documentLocation: 'https://localhost'
-      })
-
-      // hit type Transaction
-      const transaction = new Transaction({
-        transactionId: 'transaction_1',
-        affiliation: 'affiliation'
-      })
-      visitor.sendHit(transaction)
+const start = async (visitor:any, index:number) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  visitor.on('ready', async (err:any) => {
+    if (err) {
+      console.log('Flagship error:', err)
+      return
     }
+    console.log('Flagship Ready')
+
+    // getFlag
+    const flag = visitor.getFlag('js-qa-app', 'test')
+
+    const value = flag.getValue(false)
+
+    if (check[value]) {
+      check[value] += 1
+    } else {
+      check[value] = 1
+    }
+
+    console.log('flag.value', value)
+
+    await flag.userExposed()
+
+    console.log('flag.userExposed')
+    // send hit
+
+    // hit type Event
+    await visitor.sendHit({
+      type: HitType.EVENT,
+      category: EventCategory.ACTION_TRACKING,
+      action: 'KPI2',
+      value: 10
+    })
+
+    console.log('hit type Event')
+
+    await flag.userExposed()
+    // hit type Page
+    await visitor.sendHit({ type: HitType.PAGE, documentLocation: 'https://www.sdk.com/abtastylab/js/151021-' + index })
+    console.log('hit type Page')
+
+    await flag.userExposed()
+    // hit type Screen
+    await visitor.sendHit({ type: HitType.SCREEN, documentLocation: 'abtastylab-js-' + index })
+
+    console.log('hit type Screen')
+
+    await flag.userExposed()
+    // hit type Transaction
+    const transaction = new Transaction({ transactionId: visitor.visitorId, affiliation: 'KPI1' })
+    await visitor.sendHit(transaction)
+
+    console.log('hit type transaction')
+
+    console.log('check', check)
+  })
+
+  await visitor.fetchFlags()
+}
+
+async function script () {
+  await sleep(2000)
+  for (let index = 0; index <= 1; index++) {
+    const visitor = Flagship.newVisitor({ visitorId: 'visitor_a', context: { qa_report: true } })
+    await start(visitor, index)
   }
-})()
+}
+
+script()
