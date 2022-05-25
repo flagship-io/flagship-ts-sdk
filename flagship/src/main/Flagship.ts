833164cb17a9a0388b714b1b4564797ef37bc9fb
Heihokon <neoshark22@gmail.com>
2022-05-25 10:58:05 +0100

    remove log

--- flagship/src/main/Flagship.ts
+++ flagship/src/main/Flagship.ts
@@ -158,7 +158,6 @@ export class Flagship {
     apiKey: string,
     config?: IFlagshipConfig | FlagshipConfig
   ): Flagship | null {
-    console.log('test deno auto build')
 
     const flagship = this.getInstance()
 
