8a5c5fb571af06ea77cbfef8ea1bc09dc87b4b4a
Heihokon <neoshark22@gmail.com>
2022-06-06 14:52:17 +0100

    add addHit and addHits methods

--- flagship/src/api/TrackingManagerAbstract.ts
+++ flagship/src/api/TrackingManagerAbstract.ts
@@ -1,33 +1,45 @@
 import { IFlagshipConfig } from '../config/FlagshipConfig'
-import { HitAbstract } from '../hit/HitAbstract'
-import { FlagDTO } from '../types'
+import { BatchStrategy } from '../enum/BatchStrategy'
+import { HitAbstract } from '../hit'
 import { IHttpClient } from '../utils/HttpClient'
-import { VisitorAbstract } from '../visitor/VisitorAbstract'
+import { CachingStrategyAbstract } from './CachingStrategyAbstract'
+import { ContinuousCachingStrategy } from './ContinuousCachingStrategy'
 
 export interface ITrackingManager {
-  /**
-   * Send to server that this user has seen this modification
-   * @param visitor
-   * @param modification
-   */
-  sendActive(visitor: VisitorAbstract, modification: FlagDTO): Promise<void>;
-  /**
-   *Send a Hit to Flagship servers for reporting.
-   * @param hit
-   */
-  sendHit(hit: HitAbstract): Promise<void>;
-
-  sendConsentHit(visitor: VisitorAbstract):Promise<void>;
 
   config:IFlagshipConfig
+
+  addHit(hit: HitAbstract): Promise<void>
+
+  addHits(hits: HitAbstract[]): Promise<void>
 }
 
 export abstract class TrackingManagerAbstract implements ITrackingManager {
   private _httpClient: IHttpClient;
   private _config: IFlagshipConfig;
+
+  private _hitsPoolQueue: Map<string, HitAbstract>;
+
+  protected strategy: CachingStrategyAbstract;
+
   constructor (httpClient: IHttpClient, config: IFlagshipConfig) {
+    this._hitsPoolQueue = new Map<string, HitAbstract>()
     this._httpClient = httpClient
     this._config = config
+    this.strategy = this.initStrategy()
+  }
+
+  initStrategy ():CachingStrategyAbstract {
+    let strategy = new ContinuousCachingStrategy(this.config, this.httpClient, this._hitsPoolQueue)
+    switch (this.config.trackingMangerConfig?.batchStrategy) {
+      case BatchStrategy.NO_BATCHING_WITH_CONTINUOUS_CACHING_STRATEGY :
+        strategy = new ContinuousCachingStrategy(this.config, this.httpClient, this._hitsPoolQueue)
+        break
+      case BatchStrategy.BATCHING_WITH_PERIODIC_CACHING_STRATEGY:
+        strategy = new ContinuousCachingStrategy(this.config, this.httpClient, this._hitsPoolQueue)
+        break
+    }
+    return strategy
   }
 
   public get httpClient ():IHttpClient {
@@ -38,12 +50,7 @@ export abstract class TrackingManagerAbstract implements ITrackingManager {
     return this._config
   }
 
-  public abstract sendActive(
-    visitor: VisitorAbstract,
-    modification: FlagDTO
-  ): Promise<void>;
+  public abstract addHit(hit: HitAbstract): Promise<void>
 
-  public abstract sendHit(hit: HitAbstract): Promise<void>;
-
-  public abstract sendConsentHit(visitor: VisitorAbstract): Promise<void>;
+  public abstract addHits(hits: HitAbstract[]): Promise<void>
 }
