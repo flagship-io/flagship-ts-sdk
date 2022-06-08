ccaca46e6e3dc7ed6ccae48c563c76e6d1d5edfd
Heihokon <neoshark22@gmail.com>
2022-06-08 12:04:03 +0100

    refactor IHitCacheImplementation interface

--- flagship/src/cache/IHitCacheImplementation.ts
+++ flagship/src/cache/IHitCacheImplementation.ts
@@ -1,15 +1,15 @@
-import { IHitAbstract } from '../hit/index'
+import { HitCacheDTO } from '../types'
 
 export interface IHitCacheImplementation {
     /**
      * This method will be called to cache visitor hits when a hit has failed to be sent if there is no internet, there has been a timeout or if the request responded with something > 2XX. */
-    cacheHit(hits: Map<string, IHitAbstract>):Promise<void>
+    cacheHit(hits: Map<string, HitCacheDTO>):Promise<void>
     /**
      * This method will be called to load hits corresponding to visitor ID from your database and trying to send them again in the background.
      * Note: Hits older than 4H will be ignored
      */
-    lookupHits():Promise<Map<string, IHitAbstract>>
+    lookupHits():Promise<Map<string, HitCacheDTO>>
     /**
      * This method will be called to erase the visitor hits cache corresponding to visitor ID from your database. */
-    flushHits(hitIds: string[]): Promise<void>
+    flushHits(hitKeys: string[]): Promise<void>
 }
