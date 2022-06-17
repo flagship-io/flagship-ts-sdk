550df5676363519981fd8eb20e898cea679fa039
Heihokon <neoshark22@gmail.com>
2022-06-17 15:06:25 +0100

    refactor Page hit constructor

--- flagship/src/hit/Page.ts
+++ flagship/src/hit/Page.ts
@@ -24,12 +24,12 @@ export class Page extends HitAbstract implements IPage {
   public constructor (page:Omit<IPage, 'type'>) {
     super({
       type: HitType.PAGE_VIEW,
-      userIp: page?.userIp,
-      screenResolution: page?.screenResolution,
-      locale: page?.locale,
-      sessionNumber: page?.sessionNumber
+      userIp: page.userIp,
+      screenResolution: page.screenResolution,
+      locale: page.locale,
+      sessionNumber: page.sessionNumber
     })
-    this.documentLocation = page?.documentLocation
+    this.documentLocation = page.documentLocation
   }
 
   public isReady (checkParent = true):boolean {
