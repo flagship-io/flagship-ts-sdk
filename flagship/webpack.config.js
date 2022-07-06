775a44c6b53d0e539163bee02a20e344da4de904
Heihokon <neoshark22@gmail.com>
2022-07-06 15:38:58 +0100

    add webpack.jamstack.js

--- flagship/webpack.config.js
+++ flagship/webpack.config.js
@@ -1,5 +1,6 @@
-'use strict';
+'use strict'
 const nodeConfig = require('./webpack/webpack.node.js')
 const browserConfig = require('./webpack/webpack.browser.js')
+const jamstack = require('./webpack/webpack.jamstack')
 
-module.exports = [nodeConfig, browserConfig];
+module.exports = [nodeConfig, browserConfig, jamstack]
