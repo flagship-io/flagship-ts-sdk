1c02aa10dbb9c98970138d6bd048fdb809adb1fc
Heihokon <neoshark22@gmail.com>
2022-07-12 10:24:24 +0100

    add sendActivate method

--- flagship/src/api/BatchingPeriodicCachingStrategy.ts
+++ flagship/src/api/BatchingPeriodicCachingStrategy.ts
@@ -1,4 +1,4 @@
-import { ADD_HIT, BATCH_MAX_SIZE, BATCH_SENT_SUCCESS, HEADER_APPLICATION_JSON, HEADER_CONTENT_TYPE, HEADER_X_API_KEY, HEADER_X_ENV_ID, HEADER_X_SDK_CLIENT, HEADER_X_SDK_VERSION, HitType, HIT_ADDED_IN_QUEUE, HIT_EVENT_URL, SDK_LANGUAGE, SDK_VERSION, SEND_BATCH } from '../enum/index'
+import { ACTIVATE_SENT_SUCCESS, ADD_HIT, BASE_API_URL, BATCH_MAX_SIZE, BATCH_SENT_SUCCESS, HEADER_APPLICATION_JSON, HEADER_CONTENT_TYPE, HEADER_X_API_KEY, HEADER_X_ENV_ID, HEADER_X_SDK_CLIENT, HEADER_X_SDK_VERSION, HitType, HIT_ADDED_IN_QUEUE, HIT_EVENT_URL, SDK_LANGUAGE, SDK_VERSION, SEND_ACTIVATE, SEND_BATCH, URL_ACTIVATE_MODIFICATION } from '../enum/index'
 import { Batch } from '../hit/Batch'
 import { HitAbstract, Consent } from '../hit/index'
 import { errorFormat, logDebug, logError, sprintf, uuidV4 } from '../utils/utils'
@@ -30,6 +30,38 @@ export class BatchingPeriodicCachingStrategy extends BatchingCachingStrategyAbst
     await this.cacheHit(this._hitsPoolQueue)
   }
 
+  async sendActivate (activateHits:HitAbstract[]):Promise<void> {
+    const url = `${BASE_API_URL}${URL_ACTIVATE_MODIFICATION}`
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const activateHeader = {
+      [HEADER_X_API_KEY]: this.config.apiKey as string,
+      [HEADER_X_SDK_CLIENT]: SDK_LANGUAGE.name,
+      [HEADER_X_SDK_VERSION]: SDK_VERSION,
+      [HEADER_CONTENT_TYPE]: HEADER_APPLICATION_JSON
+    }
+
+    for (const activateHit of activateHits) {
+      this._hitsPoolQueue.delete(activateHit.key)
+      const activateBody = activateHit.toApiKeys()
+
+      try {
+        await this._httpClient.postAsync(url, {
+          headers: activateHeader,
+          body: activateBody
+        })
+        logDebug(this.config, sprintf(ACTIVATE_SENT_SUCCESS, JSON.stringify(activateBody)), SEND_ACTIVATE)
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (error:any) {
+        this._hitsPoolQueue.set(activateHit.key, activateHit)
+        logError(this.config, errorFormat(error.message || error, {
+          url: HIT_EVENT_URL,
+          activateHeader,
+          body: activateBody
+        }), SEND_ACTIVATE)
+      }
+    }
+  }
+
   async sendBatch (): Promise<void> {
     const headers = {
       [HEADER_X_API_KEY]: `${this.config.apiKey}`,
@@ -44,17 +76,28 @@ export class BatchingPeriodicCachingStrategy extends BatchingCachingStrategyAbst
 
     let batchSize = 0
     let count = 0
+    const activateHits:HitAbstract[] = []
 
-    this._hitsPoolQueue.forEach((item) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    for (const [_, item] of this._hitsPoolQueue) {
+      if (item.type === 'ACTIVATE') {
+        activateHits.push(item)
+        continue
+      }
       count++
       batchSize = JSON.stringify(batch).length
       if (batchSize > BATCH_MAX_SIZE || (this.config.trackingMangerConfig?.batchLength && count > this.config.trackingMangerConfig.batchLength)) {
-        return
+        break
       }
       batch.hits.push(item)
-    })
+    }
+
+    await this.sendActivate(activateHits)
 
     if (!batch.hits.length) {
+      if (activateHits.length) {
+        await this.cacheHit(this._hitsPoolQueue)
+      }
       return
     }
 
