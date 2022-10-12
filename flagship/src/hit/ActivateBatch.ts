9348701f525aef614af7aaf7456dcdff27f929fa
Heihokon <neoshark22@gmail.com>
2022-10-12 15:36:21 +0100

    add ActivateBatch class

---
+++ flagship/src/hit/ActivateBatch.ts
@@ -0,0 +1,32 @@
+import { IFlagshipConfig } from '../config'
+import { BATCH, CUSTOMER_ENV_ID_API_ITEM } from '../enum'
+import { Activate } from './Activate'
+
+export class ActivateBatch {
+  private readonly _hits : Activate[]
+  private readonly _config : IFlagshipConfig
+
+  public get hits () : Activate[] {
+    return this._hits
+  }
+
+  public get config () : IFlagshipConfig {
+    return this._config
+  }
+
+  constructor (hits:Activate[], config:IFlagshipConfig) {
+    this._config = config
+    this._hits = hits
+  }
+
+  public toApiKeys ():Record<string, unknown> {
+    return {
+      [CUSTOMER_ENV_ID_API_ITEM]: `${this.config?.envId}`,
+      [BATCH]: this.hits.map(x => {
+        const apiKeys = x.toApiKeys()
+        delete apiKeys[CUSTOMER_ENV_ID_API_ITEM]
+        return apiKeys
+      })
+    }
+  }
+}
