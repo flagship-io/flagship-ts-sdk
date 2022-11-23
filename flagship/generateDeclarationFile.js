530c634885fd1d02aff6ca8f49c101a280719e0b
Heihokon <neoshark22@gmail.com>
2022-11-23 08:50:15 +0100

    fix generate declation scrip

--- flagship/generateDeclarationFile.js
+++ flagship/generateDeclarationFile.js
@@ -4,4 +4,4 @@ const distDir = './dist'
 const indexDTsFile = 'index.d.ts'
 copyFileSync(distDir + '/' + indexDTsFile, distDir + '/index.lite.d.ts')
 copyFileSync(distDir + '/' + indexDTsFile, distDir + '/index.browser.d.ts')
-copyFileSync(distDir + '/' + indexDTsFile, distDir + '/index.deno.d.ts')
+copyFileSync(distDir + '/' + indexDTsFile, distDir + '/index.node.d.ts')
