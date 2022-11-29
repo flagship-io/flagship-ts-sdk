b6a042ee725b48c05d796ca24b4f4f5e67c1609e
Heihokon <neoshark22@gmail.com>
2022-11-29 14:54:23 +0100

    demo

--- flagship/demo/web/trackingManager.js
+++ flagship/demo/web/trackingManager.js
@@ -1,4 +1,4 @@
-// import { DecisionMode, EventCategory, Flagship, HitType, BatchStrategy } from '../../'
+// import { DecisionMode, EventCategory, Flagship, HitType, CacheStrategy } from '../../'
 
 const ENV_ID = ''
 const API_KEY = ''
@@ -8,10 +8,14 @@ const btnAction1 = document.getElementById('btn-action-1')
 btnAction1.addEventListener('click', async () => {
 // Initialize the SDK and send Initialize monitoring hit
   Flagship.start(ENV_ID, API_KEY, {
+    // decisionMode: DecisionMode.BUCKETING,
+    // initialBucketing: bucketing
+    // disableCache: true,
+    fetchNow: false,
     trackingMangerConfig: {
-      // cacheStrategy: CacheStrategy.PERIODIC_CACHING,
+      // cacheStrategy: 3,
       poolMaxSize: 5,
-      batchIntervals: 10
+      batchIntervals: 30
     }
   })
 })
@@ -29,49 +33,44 @@ btnAction2.addEventListener('click', async () => {
       testing_tracking_manager: true
     }
   })
-
-  // Fetch flags
-  await visitor.fetchFlags()
-
-  // Send an activate hit
-  const value = visitor.getFlag('my_flag', 'defaultValue').getValue()
-
-  console.log('flag value', value)
-
-  // Send a screen hit
-
-  for (let index = 0; index < 25; index++) {
-    await visitor.sendHit({ type: HitType.SCREEN, documentLocation: 'Screen ' + index })
-  }
-
-  // await visitor.sendHit({ type: HitType.SCREEN, documentLocation: 'Screen 1' })
-  // await visitor.sendHit({ type: HitType.SCREEN, documentLocation: 'Screen 2' })
-  // await visitor.sendHit({ type: HitType.SCREEN, documentLocation: 'Screen 3' })
-  // await visitor.sendHit({ type: HitType.SCREEN, documentLocation: 'Screen 4' })
-  // await visitor.sendHit({ type: HitType.SCREEN, documentLocation: 'Screen 5' })
-
-  // // send a page hit
-  // await visitor.sendHit({ type: HitType.PAGE, documentLocation: 'home page' })
-
-  // // Send an event hit
-  // await visitor.sendHit({ type: HitType.EVENT, action: 'click', category: EventCategory.ACTION_TRACKING })
 })
 
 const btnAction3 = document.getElementById('btn-action-3')
 
 btnAction3.addEventListener('click', async () => {
-  // visitor.setConsent(false)
-  const value = visitor.getFlag('my_flag', 'defaultValue').getValue()
-
-  console.log('flag value', value)
-  await visitor.sendHit({ type: HitType.SCREEN, documentLocation: 'Screen 1' })
-  await visitor.sendHit({ type: HitType.SCREEN, documentLocation: 'Screen 2' })
-  // await visitor.sendHit({ type: HitType.SCREEN, documentLocation: 'Screen 3' })
+  // Fetch flags
+  await visitor.fetchFlags()
 })
 
 const btnAction4 = document.getElementById('btn-action-4')
 
+// scenario 1 action 1
 btnAction4.addEventListener('click', async () => {
+  // Send an activate hit
+  const value = visitor.getFlag('my_flag', 'defaultValue').getValue()
+
+  console.log('flag value', value)
+})
+
+const btnAction5 = document.getElementById('btn-action-5')
+
+btnAction5.addEventListener('click', async () => {
+  visitor.setConsent(false)
+})
+
+const btnAction6 = document.getElementById('btn-action-6')
+
+btnAction6.addEventListener('click', async () => {
+  await visitor.sendHit({ type: HitType.SCREEN, documentLocation: 'Screen 1' })
+  // await visitor.sendHit({ type: HitType.SCREEN, documentLocation: 'Screen 2' })
+  // await visitor.sendHit({ type: HitType.SCREEN, documentLocation: 'Screen 3' })
+  // await visitor.sendHit({ type: HitType.SCREEN, documentLocation: 'Screen 4' })
+  // await visitor.sendHit({ type: HitType.SCREEN, documentLocation: 'Screen 5' })
+})
+
+const btnAction7 = document.getElementById('btn-action-7')
+
+btnAction7.addEventListener('click', async () => {
   const visitor2 = Flagship.newVisitor({
     visitorId: 'visitor-B',
     context: {
@@ -84,4 +83,10 @@ btnAction4.addEventListener('click', async () => {
   const value = visitor2.getFlag('my_flag', 'defaultValue').getValue()
   console.log('flag value', value)
   await visitor2.sendHit({ type: HitType.SCREEN, documentLocation: 'Screen 1' })
+})
+
+const btnAction8 = document.getElementById('btn-action-8')
+
+btnAction8.addEventListener('click', async () => {
+  await Flagship.close()
 })
