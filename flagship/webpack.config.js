ec9ad80b02632f376e453f7ed6a170ccd339d83a
Heihokon <neoshark22@gmail.com>
2023-02-02 14:13:35 +0100

    remove unused file

--- flagship/webpack.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-'use strict'
-const nodeConfig = require('./webpack/webpack.node.js')
-const browserConfig = require('./webpack/webpack.browser.js')
-const browserLiteConfig = require('./webpack/webpack.browser.lite.js')
-
-module.exports = [nodeConfig, browserConfig, browserLiteConfig]
