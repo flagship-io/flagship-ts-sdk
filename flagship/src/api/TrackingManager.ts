5f697630d6d6d16d0c41efbfa1f3decf59a0a1a9
Heihokon <neoshark22@gmail.com>
2023-04-14 16:11:34 -0500

    add monitoring send hit

--- flagship/src/api/TrackingManager.ts
+++ flagship/src/api/TrackingManager.ts
@@ -1,6 +1,8 @@
+import { LogLevel } from '../enum'
 import { BatchTriggeredBy } from '../enum/BatchTriggeredBy'
 import { Activate } from '../hit/Activate'
 import { HitAbstract } from '../hit/index'
+import { Monitoring } from '../hit/Monitoring'
 import { TrackingManagerAbstract } from './TrackingManagerAbstract'
 
 export class TrackingManager extends TrackingManagerAbstract {
@@ -10,6 +12,18 @@ export class TrackingManager extends TrackingManagerAbstract {
 
   public async addHit (hit: HitAbstract): Promise<void> {
     await this.strategy.addHit(hit)
+    const monitoring = new Monitoring({
+      type: 'TROUBLESHOOTING',
+      subComponent: 'VISITOR-SEND-HIT',
+      logLevel: LogLevel.INFO,
+      message: 'VISITOR-SEND-HIT',
+      visitorId: hit.visitorId,
+      anonymousId: hit.anonymousId,
+      config: this.config,
+      hitContent: hit.toApiKeys()
+    })
+
+    this.addMonitoringHit(monitoring)
   }
 
   public async sendBatch (): Promise<void> {
