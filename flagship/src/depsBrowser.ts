392e512a42070db7ff144c60a1c6aa32a1d5891d
Heihokon <neoshark22@gmail.com>
2023-04-21 17:00:23 -0500

    refactor fetch browser

--- flagship/src/depsBrowser.ts
+++ flagship/src/depsBrowser.ts
@@ -1,3 +1,13 @@
-export const fetch = (input: RequestInfo | URL, init?: RequestInit) => globalThis.fetch(input, init)
+export const fetch = (input: RequestInfo | URL, init?: RequestInit) => {
+  if (globalThis) {
+    return globalThis.fetch(input, init)
+  }
+  if (window) {
+    return window.fetch(input, init)
+  }
+  if (self) {
+    return self.fetch(input, init)
+  }
+}
 
 export { EventEmitter } from 'events'
