edc3ed8b5f24abfd5bde9ac1aeeac203e7d1cf82
Heihokon <Heihokon@users.noreply.github.com>
2023-05-08 18:50:38 +0000

    build deno module

--- flagship/dist-deno/src/depsNode.ts
+++ flagship/dist-deno/src/depsNode.ts
@@ -1,12 +1,10 @@
+import { Agent as HttpAgent } from 'http.ts'
+import { Agent as HttpAgents } from 'https.ts'
 import fetch from 'node-fetch'
 
 function getHttpAgent ():Record<string, unknown> {
   const globalOption:Record<string, unknown> = {}
   if (typeof window === 'undefined') {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const { Agent: HttpAgent } = require('http')
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const { Agent: HttpAgents } = require('https')
     globalOption.agent = (parsedURL:URL) => {
       return parsedURL.protocol === 'http:' ? new HttpAgent({ keepAlive: true }) : new HttpAgents({ keepAlive: true })
     }
