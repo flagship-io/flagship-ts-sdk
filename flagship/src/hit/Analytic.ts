f7c7ff08c675a323b31bec30e5b8fb749537c2a6
Heihokon <neoshark22@gmail.com>
2023-07-27 08:57:33 -0500

    add Analytic hit

---
+++ flagship/src/hit/Analytic.ts
@@ -0,0 +1,10 @@
+import { IFlagshipConfig } from '../config/IFlagshipConfig'
+import { Diagnostic, IDiagnostic } from './Diagnostic'
+
+export type AnalyticType = Omit<IDiagnostic & {config: IFlagshipConfig}, 'createdAt'|'category'|'type'>
+
+export class Analytic extends Diagnostic {
+  public constructor (param:AnalyticType) {
+    super({ ...param, type: 'USAGE' })
+  }
+}
