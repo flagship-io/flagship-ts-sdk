4d407252f8062502cdff9b90ac5042c4a56bafd8
Heihokon <neoshark22@gmail.com>
2023-09-18 15:17:41 -0500

    enable sendAnalyticHit in NoConsent strategy

--- flagship/src/visitor/NoConsentStrategy.ts
+++ flagship/src/visitor/NoConsentStrategy.ts
@@ -6,7 +6,6 @@ import { DefaultStrategy } from './DefaultStrategy'
 import { CampaignDTO } from '../decision/api/models'
 import { BatchDTO } from '../hit/Batch'
 import { Troubleshooting } from '../hit/Troubleshooting'
-import { Analytic } from '../hit/Analytic'
 
 export class NoConsentStrategy extends DefaultStrategy {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -57,11 +56,6 @@ export class NoConsentStrategy extends DefaultStrategy {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   public sendTroubleshootingHit (_hit: Troubleshooting): Promise<void> {
-    return Promise.resolve()
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  public async sendAnalyticHit (_hit: Analytic) {
     return Promise.resolve()
   }
 
