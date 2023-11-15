77d8d6c0ee5ab22716f696a85cd823d0badf70b3
Heihokon <neoshark22@gmail.com>
2023-11-14 19:22:56 -0600

    implement launchQaAssistant function

---
+++ flagship/src/qaAssistant/index.ts
@@ -0,0 +1,75 @@
+import { IFlagshipConfig } from '../config/IFlagshipConfig'
+import { isBrowser, logInfoSprintf } from '../utils/utils'
+import { handleIframeMessage } from './handleIframeMessage'
+import { EventDataFromIframe } from './type'
+
+/**
+ *
+ * @param bundleFileUrl
+ */
+function appendScript (bundleFileUrl: string): void {
+  const script = document.createElement('script')
+  script.src = bundleFileUrl
+  document.body.append(script)
+}
+
+/**
+ *
+ * @param config
+ * @returns
+ */
+function loadQaAssistant (config: IFlagshipConfig): void {
+  if (window.frames.ABTastyQaAssistant) {
+    return
+  }
+
+  logInfoSprintf(config, 'QA assistant', 'Loading QA Assistant')
+  //   const bundleFileUrl = 'https://qa-assistant.abtasty.com/bundle.js'
+  const bundleFileUrl = 'https://127.0.0.1:5000/bundle.js'
+  appendScript(bundleFileUrl)
+  window.flagship = {
+    envId: config.envId as string
+  }
+}
+
+/**
+ *
+ * @param config
+ */
+function listenForKeyboardQaAssistant (config: IFlagshipConfig) {
+  logInfoSprintf(config, 'QA assistant', 'Listening for keyboard events to launch QA Assistant')
+
+  const keysPressed: Record<string, boolean> = {}
+
+  const keyCombinationPressed = (): boolean => {
+    return (keysPressed.Control || keysPressed.Alt) && keysPressed.q && keysPressed.a
+  }
+
+  document.addEventListener('keydown', (event: KeyboardEvent) => {
+    keysPressed[event.key] = true
+    if (keyCombinationPressed()) {
+      loadQaAssistant(config)
+    }
+  })
+
+  document.addEventListener('keyup', (event: KeyboardEvent) => {
+    delete keysPressed[event.key]
+  })
+}
+
+/**
+ *
+ * @param config
+ * @returns
+ */
+export function launchQaAssistant (config: IFlagshipConfig): void {
+  if (!isBrowser()) {
+    return
+  }
+
+  window.addEventListener('message', (event: MessageEvent<EventDataFromIframe>) => {
+    handleIframeMessage({ event, config })
+  })
+
+  listenForKeyboardQaAssistant(config)
+}
