fdb834c53f1ace398fd0669a7b819a360b5ad166
Heihokon <neoshark22@gmail.com>
2023-11-14 19:23:16 -0600

    add handleIframeMessage message

---
+++ flagship/src/qaAssistant/handleIframeMessage.ts
@@ -0,0 +1,15 @@
+
+import { IFlagshipConfig } from '../config/IFlagshipConfig'
+import { onQaAssistantReady } from './messages'
+import { EventDataFromIframe, MSG_NAME_FROM_IFRAME } from './type'
+
+export function handleIframeMessage ({ event, config }: { event: MessageEvent<EventDataFromIframe>, config: IFlagshipConfig }) {
+  switch (event.data.name) {
+    case MSG_NAME_FROM_IFRAME.QA_ASSISTANT_IS_READY:
+      onQaAssistantReady(config)
+      break
+
+    default:
+      break
+  }
+}
