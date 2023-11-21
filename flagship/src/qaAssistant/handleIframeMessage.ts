47e144074f387df8a3796fd4b782599c7f61b96a
Heihokon <neoshark22@gmail.com>
2023-11-21 17:38:22 -0600

    add onQaAssistantClose function

--- flagship/src/qaAssistant/handleIframeMessage.ts
+++ flagship/src/qaAssistant/handleIframeMessage.ts
@@ -1,14 +1,23 @@
 
 import { IFlagshipConfig } from '../config/IFlagshipConfig'
+import { FS_IS_QA_MODE_ENABLED } from '../enum/FlagshipConstant'
 import { EventDataFromIframe, MSG_NAME_FROM_IFRAME } from './type'
 
-export function handleIframeMessage ({ event, config }: { event: MessageEvent<EventDataFromIframe>, config: IFlagshipConfig }) {
+export function handleIframeMessage ({ event, config, func }: { event: MessageEvent<EventDataFromIframe>, config: IFlagshipConfig, func?: (event: MessageEvent<EventDataFromIframe>) => void }) {
   switch (event.data.name) {
-    case MSG_NAME_FROM_IFRAME.QA_ASSISTANT_IS_READY:
-
+    case MSG_NAME_FROM_IFRAME.QaAssistantClose:
+      onQaAssistantClose({ config, func })
       break
 
     default:
       break
+  }
+}
+
+function onQaAssistantClose ({ config, func }:{config:IFlagshipConfig, func?: (event: MessageEvent<EventDataFromIframe>) => void}) {
+  config.isQAModeEnabled = false
+  sessionStorage.removeItem(FS_IS_QA_MODE_ENABLED)
+  if (func) {
+    window.removeEventListener('message', func)
   }
 }
