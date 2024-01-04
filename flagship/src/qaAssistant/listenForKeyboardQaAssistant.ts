4523b9f0c7e57560f029e0705c368ce906de69fb
Heihokon <neoshark22@gmail.com>
2024-01-04 14:58:35 -0600

    add listenForKeyboardQaAssistant

---
+++ flagship/src/qaAssistant/listenForKeyboardQaAssistant.ts
@@ -0,0 +1,30 @@
+import { IFlagshipConfig } from '../config/IFlagshipConfig'
+import { FS_FORCED_VARIATIONS } from '../enum/FlagshipConstant'
+import { logInfoSprintf } from '../utils/utils'
+import { loadQaAssistant } from './loadQaAssistant'
+
+/**
+ *
+ * @param config
+ */
+export function listenForKeyboardQaAssistant (config: IFlagshipConfig) {
+  logInfoSprintf(config, 'QA assistant', 'Listening for keyboard events to launch QA Assistant')
+
+  const keysPressed: Record<string, boolean> = {}
+
+  const keyCombinationPressed = (): boolean => {
+    return (keysPressed.Control || keysPressed.Alt) && keysPressed.q && keysPressed.a
+  }
+
+  document.addEventListener('keydown', (event: KeyboardEvent) => {
+    keysPressed[event.key] = true
+    sessionStorage.removeItem(FS_FORCED_VARIATIONS)
+    if (keyCombinationPressed()) {
+      loadQaAssistant(config)
+    }
+  })
+
+  document.addEventListener('keyup', (event: KeyboardEvent) => {
+    delete keysPressed[event.key]
+  })
+}
