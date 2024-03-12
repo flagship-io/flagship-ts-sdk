d0e55d4e11bce5744153cac538a7da260b3032c3
Heihokon <neoshark22@gmail.com>
2024-03-12 12:33:35 -0500

    Update VisitorStrategy.test.ts with new enum and logWarning

--- flagship/test/visitor/VisitorStrategy.test.ts
+++ flagship/test/visitor/VisitorStrategy.test.ts
@@ -3,7 +3,7 @@ import { DecisionApiConfig, FlagshipStatus } from '../../src'
 import { TrackingManager } from '../../src/api/TrackingManager'
 import { ConfigManager } from '../../src/config'
 import { ApiManager } from '../../src/decision/ApiManager'
-import { ACTIVATE_MODIFICATION_ERROR, METHOD_DEACTIVATED_CONSENT_ERROR, METHOD_DEACTIVATED_ERROR, PROCESS_ACTIVE_MODIFICATION } from '../../src/enum'
+import { FLAG_USER_EXPOSED, METHOD_DEACTIVATED_CONSENT_ERROR, METHOD_DEACTIVATED_ERROR, USER_EXPOSED_FLAG_ERROR } from '../../src/enum'
 import { FlagshipLogManager } from '../../src/utils/FlagshipLogManager'
 import { IHttpClient } from '../../src/utils/HttpClient'
 import { sprintf } from '../../src/utils/utils'
@@ -20,6 +20,7 @@ describe('test getStrategy', () => {
   const logManager = new FlagshipLogManager()
   const logError = jest.spyOn(logManager, 'error')
   const logInfo = jest.spyOn(logManager, 'info')
+  const logWarning = jest.spyOn(logManager, 'warning')
 
   const config = new DecisionApiConfig({ envId: 'envId', apiKey: 'apiKey' })
   config.logManager = logManager
@@ -27,46 +28,42 @@ describe('test getStrategy', () => {
   const trackingManager = new TrackingManager({} as IHttpClient, config)
   const configManager = new ConfigManager(config, {} as ApiManager, trackingManager)
 
-  const visitorDelegate = new VisitorDelegate({ visitorId, context, hasConsented: false, configManager: configManager as ConfigManager })
+  const visitorDelegate = new VisitorDelegate({ visitorId, context, hasConsented: true, configManager: configManager as ConfigManager })
 
   it('test NotReadyStrategy flagship status is undefined', async () => {
-    const methodName = 'activateModification'
-    await visitorDelegate.activateModification('key')
+    await visitorDelegate.visitorExposed({ key: 'key', defaultValue: 'defaultValue' })
     expect(logError).toBeCalledTimes(1)
-    expect(logError).toBeCalledWith(sprintf(METHOD_DEACTIVATED_ERROR, visitorId, methodName, FlagshipStatus[FlagshipStatus.NOT_INITIALIZED]), methodName)
+    expect(logError).toBeCalledWith(sprintf(METHOD_DEACTIVATED_ERROR, visitorId, FLAG_USER_EXPOSED, FlagshipStatus[FlagshipStatus.NOT_INITIALIZED]), FLAG_USER_EXPOSED)
   })
 
   it('test NotReadyStrategy flagship with status  NOT_INITIALIZED', async () => {
     VisitorAbstract.SdkStatus = FlagshipStatus.NOT_INITIALIZED
-    const methodName = 'activateModification'
-    await visitorDelegate.activateModification('key')
+    await visitorDelegate.visitorExposed({ key: 'key', defaultValue: 'defaultValue' })
     expect(logError).toBeCalledTimes(1)
-    expect(logError).toBeCalledWith(sprintf(METHOD_DEACTIVATED_ERROR, visitorId, methodName, FlagshipStatus[FlagshipStatus.NOT_INITIALIZED]), methodName)
+    expect(logError).toBeCalledWith(sprintf(METHOD_DEACTIVATED_ERROR, visitorId, FLAG_USER_EXPOSED, FlagshipStatus[FlagshipStatus.NOT_INITIALIZED]), FLAG_USER_EXPOSED)
   })
 
   it('test PanicStrategy', async () => {
     VisitorAbstract.SdkStatus = FlagshipStatus.READY_PANIC_ON
-    const methodName = 'activateModification'
-    await visitorDelegate.activateModification('key')
-    await visitorDelegate.activateModification('key')
-    expect(logInfo).toBeCalledTimes(2)
-    expect(logInfo).toBeCalledWith(sprintf(METHOD_DEACTIVATED_ERROR, visitorId, methodName, FlagshipStatus[FlagshipStatus.READY_PANIC_ON]), methodName)
+    await visitorDelegate.visitorExposed({ key: 'key', defaultValue: 'defaultValue' })
+    expect(logInfo).toBeCalledTimes(1)
+    expect(logInfo).toBeCalledWith(sprintf(METHOD_DEACTIVATED_ERROR, visitorId, FLAG_USER_EXPOSED, FlagshipStatus[FlagshipStatus.READY_PANIC_ON]), FLAG_USER_EXPOSED)
   })
 
   it('test NoConsent', async () => {
+    visitorDelegate.hasConsented = false
     VisitorAbstract.SdkStatus = FlagshipStatus.READY
-    const methodName = 'activateModification'
-    await visitorDelegate.activateModification('key')
+    await visitorDelegate.visitorExposed({ key: 'key', defaultValue: 'defaultValue' })
     expect(logInfo).toBeCalledTimes(1)
-    expect(logInfo).toBeCalledWith(sprintf(METHOD_DEACTIVATED_CONSENT_ERROR, methodName, visitorDelegate.visitorId), methodName)
+    expect(logInfo).toBeCalledWith(sprintf(METHOD_DEACTIVATED_CONSENT_ERROR, FLAG_USER_EXPOSED, visitorDelegate.visitorId), FLAG_USER_EXPOSED)
+    visitorDelegate.hasConsented = true
   })
 
   it('test DefaultStrategy', async () => {
     VisitorAbstract.SdkStatus = FlagshipStatus.READY
-    visitorDelegate.setConsent(true)
-    await visitorDelegate.activateModification('key')
-    expect(logError).toBeCalledTimes(1)
-    expect(logError).toBeCalledWith(sprintf(ACTIVATE_MODIFICATION_ERROR, 'key'),
-      PROCESS_ACTIVE_MODIFICATION)
+    await visitorDelegate.visitorExposed({ key: 'key', defaultValue: 'defaultValue' })
+    expect(logWarning).toBeCalledTimes(1)
+    expect(logWarning).toBeCalledWith(sprintf(USER_EXPOSED_FLAG_ERROR, visitorId, 'key'),
+      FLAG_USER_EXPOSED)
   })
 })
