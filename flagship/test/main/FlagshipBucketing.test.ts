8727102b804f8ea28168369eff278be93e4da859
Heihokon <neoshark22@gmail.com>
2024-03-12 12:27:42 -0500

    Update FlagshipBucketing.test.ts with newVisitor changes

--- flagship/test/main/FlagshipBucketing.test.ts
+++ flagship/test/main/FlagshipBucketing.test.ts
@@ -1,5 +1,5 @@
 import { jest, expect, it, describe } from '@jest/globals'
-import { Flagship, DecisionMode, Visitor } from '../../src'
+import { Flagship, DecisionMode, Visitor, NewVisitor } from '../../src'
 import { IFlagshipConfig } from '../../src/config'
 import { MurmurHash } from '../../src/utils/MurmurHash'
 import { HttpClient } from '../../src/utils/HttpClient'
@@ -58,17 +58,17 @@ describe('test start in Bucketing mode', () => {
         key: 'value'
       }
       const visitorId = 'visitorId'
-      let visitor = instance.newVisitor(visitorId, context)
+      let visitor = instance.newVisitor({ visitorId, context, hasConsented: true })
       expect(visitor).toBeInstanceOf(Visitor)
       if (visitor) {
         expect(visitor.context).toEqual(expect.objectContaining(context))
         expect(visitor.visitorId).toBe(visitorId)
       }
 
-      visitor = instance.newVisitor({})
+      visitor = instance.newVisitor({} as NewVisitor)
       expect(visitor).toBeInstanceOf(Visitor)
 
-      visitor = instance.newVisitor(visitorId)
+      visitor = instance.newVisitor({ visitorId, hasConsented: true })
       expect(visitor).toBeInstanceOf(Visitor)
       if (visitor) {
         expect(visitor.visitorId).toBe(visitorId)
