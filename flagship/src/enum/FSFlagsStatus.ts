c35f4a511d9d75ce7a34a433248fba11362ae390
Heihokon <neoshark22@gmail.com>
2024-03-20 15:05:42 -0500

    Add FSFlagsStatus enum to represent the status of flags in the Flagship SDK

---
+++ flagship/src/enum/FSFlagsStatus.ts
@@ -0,0 +1,26 @@
+
+
+/**
+ * Represents the status of the flags in the Flagship SDK.
+ */
+export enum FSFlagsStatus {
+    /**
+     * The flags have been successfully fetched from the API or re-evaluated in bucketing mode.
+     */
+    FETCHED = 'FETCHED',
+
+    /**
+     * The flags are currently being fetched from the API or re-evaluated in bucketing mode.
+     */
+    FETCHING = 'FETCHING',
+
+    /**
+     * The flags need to be re-fetched due to a change in context, or because the flags were loaded from cache or XPC.
+     */
+    FETCH_REQUIRED = 'FETCH_REQUIRED',
+
+    /**
+     * The SDK is in PANIC mode: All features are disabled except for the one to fetch flags.
+     */
+    PANIC = 'PANIC',
+}
