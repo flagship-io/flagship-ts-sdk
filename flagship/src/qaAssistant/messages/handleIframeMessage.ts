ead2df6d5060294f9be9fee24d68a5ae88b042ef
Heihokon <neoshark22@gmail.com>
2024-04-12 15:01:36 -0500

    Add render function to handleIframeMessage

--- flagship/src/qaAssistant/messages/handleIframeMessage.ts
+++ flagship/src/qaAssistant/messages/handleIframeMessage.ts
@@ -2,7 +2,7 @@
 import { IFlagshipConfig } from '../../config/IFlagshipConfig'
 import { isBrowser } from '../../utils/utils'
 import { EventDataFromIframe, MSG_NAME_FROM_IFRAME } from '../type'
-import { onApplyForcedVariations, onQaAssistantClose, onQaAssistantReady, onResetForcedVariations } from './iframeMessageActions'
+import { onApplyForcedVariations, onQaAssistantClose, onQaAssistantReady, onResetForcedVariations, render } from './iframeMessageActions'
 
 export function handleIframeMessage ({ event, config, func }: { event: MessageEvent<EventDataFromIframe>, config: IFlagshipConfig, func?: (event: MessageEvent<EventDataFromIframe>) => void }) {
   if (!config.isQAModeEnabled || !isBrowser()) {
@@ -21,6 +21,9 @@ export function handleIframeMessage ({ event, config, func }: { event: MessageEv
       break
     case MSG_NAME_FROM_IFRAME.FsResetForcedVariations:
       onResetForcedVariations()
+      break
+    case MSG_NAME_FROM_IFRAME.FsTriggerRender:
+      render()
       break
   }
 }
