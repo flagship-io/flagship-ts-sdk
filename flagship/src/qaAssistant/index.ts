60e50fdef6c661f0aa6d2bef385bcf0b0da740e7
Heihokon <neoshark22@gmail.com>
2024-04-17 15:35:21 -0500

    Update QA Assistant URLs and launch logic in index.ts

--- flagship/src/qaAssistant/index.ts
+++ flagship/src/qaAssistant/index.ts
@@ -1,3 +1,4 @@
+import { FS_QA_ASSISTANT, FS_QA_ASSISTANT_LOCAL, FS_QA_ASSISTANT_STAGING, QA_ASSISTANT_LOCAL_URL, QA_ASSISTANT_PROD_URL, QA_ASSISTANT_STAGING_URL } from '@src/enum/FlagshipConstant'
 import { IFlagshipConfig } from '../config/IFlagshipConfig'
 import { isBrowser } from '../utils/utils'
 import { listenForKeyboardQaAssistant } from './listenForKeyboardQaAssistant'
@@ -12,8 +13,19 @@ export function launchQaAssistant (config: IFlagshipConfig): void {
   if (!isBrowser()) {
     return
   }
-  if (config.isQAModeEnabled) {
-    loadQaAssistant(config)
+
+  let bundleUrl:string|null = null
+  const queryParam = new URLSearchParams(window.location.search)
+  if (queryParam.get(FS_QA_ASSISTANT) === 'true') {
+    bundleUrl = QA_ASSISTANT_PROD_URL
+  } else if (queryParam.get(FS_QA_ASSISTANT_STAGING) === 'true') {
+    bundleUrl = QA_ASSISTANT_STAGING_URL
+  } else if (queryParam.get(FS_QA_ASSISTANT_LOCAL) === 'true') {
+    bundleUrl = QA_ASSISTANT_LOCAL_URL
+  }
+
+  if (config.isQAModeEnabled || bundleUrl) {
+    loadQaAssistant(config, bundleUrl)
     return
   }
   listenForKeyboardQaAssistant(config)
