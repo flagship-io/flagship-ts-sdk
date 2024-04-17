b9f00036d5899c0bdae9889673ac5a17bc5da09e
Heihokon <neoshark22@gmail.com>
2024-04-17 15:55:39 -0500

    Update QA Assistant URLs in appendScript.test.ts

--- flagship/test/qaAssistant/appendScript.test.ts
+++ flagship/test/qaAssistant/appendScript.test.ts
@@ -5,18 +5,18 @@
 import { describe, it, expect } from '@jest/globals'
 import { DecisionApiConfig } from '../../src/config/DecisionApiConfig'
 import { appendScript } from '../../src/qaAssistant/appendScript'
-import { FS_QA_ASSISTANT_SCRIPT_TAG_ID, QA_ASSISTANT_URL } from '../../src/enum/FlagshipConstant'
+import { FS_QA_ASSISTANT_SCRIPT_TAG_ID, QA_ASSISTANT_PROD_URL } from '../../src/enum/FlagshipConstant'
 
 describe('Test appendScript', () => {
   const config = new DecisionApiConfig()
   config.envId = 'envId'
 
   it('test Script is appended to document', () => {
-    appendScript(QA_ASSISTANT_URL)
+    appendScript(QA_ASSISTANT_PROD_URL)
 
     const tagScript = document.getElementById(FS_QA_ASSISTANT_SCRIPT_TAG_ID) as HTMLScriptElement|null
     expect(tagScript).toBeDefined()
-    expect(tagScript?.src).toBe(QA_ASSISTANT_URL)
+    expect(tagScript?.src).toBe(QA_ASSISTANT_PROD_URL)
     expect(tagScript).toMatchSnapshot()
   })
 })
