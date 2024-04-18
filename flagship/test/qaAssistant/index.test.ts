dcd00b26619e04fe9f4d3a43e5220210a08d63d2
Heihokon <neoshark22@gmail.com>
2024-04-18 09:12:50 -0500

    Update QA Assistant URLs and launch logic in index.test.ts

--- flagship/test/qaAssistant/index.test.ts
+++ flagship/test/qaAssistant/index.test.ts
@@ -1,10 +1,16 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import { jest, describe, beforeEach, it, expect } from '@jest/globals'
 import * as utils from '../../src/utils/utils'
 import { launchQaAssistant } from '../../src/qaAssistant'
-import { DecisionApiConfig } from '../../src/mod'
 import * as loadQaAssistant from '../../src/qaAssistant/loadQaAssistant'
 import * as listenForKeyboardQaAssistant from '../../src/qaAssistant/listenForKeyboardQaAssistant'
+import { DecisionApiConfig } from '@src/config/DecisionApiConfig'
+import { FS_QA_ASSISTANT, FS_QA_ASSISTANT_LOCAL, FS_QA_ASSISTANT_STAGING, QA_ASSISTANT_LOCAL_URL, QA_ASSISTANT_PROD_URL, QA_ASSISTANT_STAGING_URL } from '@src/enum/FlagshipConstant'
 describe('Qa Assistant', () => {
+  const { location } = window
   beforeEach(() => {
     isBrowserSpy.mockReturnValue(true)
 
@@ -14,6 +20,10 @@ describe('Qa Assistant', () => {
     listenForKeyboardQaAssistantSpy.mockImplementation(() => {
       //
     })
+  })
+
+  afterAll(() => {
+    window.location = location
   })
 
   const isBrowserSpy = jest.spyOn(utils, 'isBrowser')
@@ -35,7 +45,7 @@ describe('Qa Assistant', () => {
 
     launchQaAssistant(config)
     expect(loadQaAssistantSpy).toBeCalledTimes(1)
-    expect(loadQaAssistantSpy).toBeCalledWith(config)
+    expect(loadQaAssistantSpy).toBeCalledWith(config, null)
     expect(listenForKeyboardQaAssistantSpy).toBeCalledTimes(0)
   })
 
@@ -47,5 +57,47 @@ describe('Qa Assistant', () => {
     expect(loadQaAssistantSpy).toBeCalledTimes(0)
     expect(listenForKeyboardQaAssistantSpy).toBeCalledTimes(1)
     expect(listenForKeyboardQaAssistantSpy).toBeCalledWith(config)
+  })
+
+  it('test launchQaAssistant when when fs_qa_assistant is true ', () => {
+    const config = new DecisionApiConfig()
+    config.isQAModeEnabled = false
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    delete (window as any).location
+    window.location = { ...location, search: `?${FS_QA_ASSISTANT}=true` }
+
+    launchQaAssistant(config)
+    expect(loadQaAssistantSpy).toBeCalledTimes(1)
+    expect(loadQaAssistantSpy).toBeCalledWith(config, QA_ASSISTANT_PROD_URL)
+    expect(listenForKeyboardQaAssistantSpy).toBeCalledTimes(0)
+  })
+
+  it('test launchQaAssistant when when fs_qa_assistant_staging is true ', () => {
+    const config = new DecisionApiConfig()
+    config.isQAModeEnabled = false
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    delete (window as any).location
+    window.location = { ...location, search: `?${FS_QA_ASSISTANT_STAGING}=true` }
+
+    launchQaAssistant(config)
+    expect(loadQaAssistantSpy).toBeCalledTimes(1)
+    expect(loadQaAssistantSpy).toBeCalledWith(config, QA_ASSISTANT_STAGING_URL)
+    expect(listenForKeyboardQaAssistantSpy).toBeCalledTimes(0)
+  })
+
+  it('test launchQaAssistant when when fs_qa_assistant_local is true ', () => {
+    const config = new DecisionApiConfig()
+    config.isQAModeEnabled = false
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    delete (window as any).location
+    window.location = { ...location, search: `?${FS_QA_ASSISTANT_LOCAL}=true` }
+
+    launchQaAssistant(config)
+    expect(loadQaAssistantSpy).toBeCalledTimes(1)
+    expect(loadQaAssistantSpy).toBeCalledWith(config, QA_ASSISTANT_LOCAL_URL)
+    expect(listenForKeyboardQaAssistantSpy).toBeCalledTimes(0)
   })
 })
