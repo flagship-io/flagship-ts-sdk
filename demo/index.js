2f84b4e273b62c4bb44432985602fcd4f458b09d
Heihokon <neoshark22@gmail.com>
2024-04-26 12:10:02 -0500

    Refactor descriptions and installation steps in fsOnboardingConfig.json

--- demo/index.js
+++ demo/index.js
@@ -1,14 +1,12 @@
-/* start demo */
-const express = require('express');
+//start demo
+// Usage: node demo/index.js
+const express = require("express");
+const { Flagship, HitType, EventCategory } = require("@flagship.io/js-sdk");
 
-const { Flagship, HitType, EventCategory } = require("@flagship.io/js-sdk")
-/* end import */
-
-/* start step-1 starting */
+// Step 1: Start the Flagship SDK by providing the environment ID and API key
 Flagship.start("<ENV_ID>", "<API_KEY>", {
-    fetchNow: false,
-})
-/* end step-1 starting */
+  fetchNow: false,
+});
 
 const app = express();
 app.use(express.json());
@@ -16,59 +14,56 @@ app.use(express.json());
 const port = 3000;
 
 async function getVisitor() {
-
-    /* start step-2 create visitor */
-    const visitor = Flagship.newVisitor({
-        visitorId: "visitor-id",
-        hasConsented: true,
-        context:{
-            fs_is_vip: true,
-        }
-    });
-    /* end step-2 create visitor */
-
-    /* start step-3 fetch flags */
-    await visitor.fetchFlags();
-    /* end step-3 fetch flags */
-    return visitor;
-}
-
-app.get('/item', async (req, res) => {
-    const visitor = await getVisitor();
-
-    /* start step-4 get flags */
-    const fsEnableDiscount = visitor.getFlag("fs_enable_discount", false);
-    const fsAddToCartBtnColor = visitor.getFlag("fs_add_to_cart_btn_color", "blue");
-
-    const fsEnableDiscountValue = fsEnableDiscount.getValue();
-    const fsAddToCartBtnColorValue = fsAddToCartBtnColor.getValue();
-    /* end step-4 get flags */
-
-    res.json({
-        item:{
-            name: "Flagship T-shirt",
-            price: 20
-        },
-        fsEnableDiscount : fsEnableDiscountValue,
-        fsAddToCartBtnColor : fsAddToCartBtnColorValue
-    });
+  // Step 2: Create a new visitor with a visitor ID and consent status
+  const visitor = Flagship.newVisitor({
+    visitorId: "visitor-id",
+    hasConsented: true,
+    context: {
+      fs_is_vip: true,
+    },
   });
 
-app.post('/add-to-cart', async (req, res) => {
-    const visitor = await getVisitor();
+  // Step 3: Fetch the flags for the visitor
+  await visitor.fetchFlags();
 
-    /* start step-5 send hit */
-    visitor.sendHit({
-        type: HitType.EVENT,
-        category: EventCategory.ACTION_TRACKING,
-        action: "add-to-cart-clicked"
-    })
-    /* end step-5 send hit */
-    res.json(null);
-})
-  
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  return visitor;
+}
+
+app.get("/item", async (req, res) => {
+  const visitor = await getVisitor();
+
+  // Step 4: Get the values of the flags for the visitor
+  const fsEnableDiscount = visitor.getFlag("fs_enable_discount", false);
+  const fsAddToCartBtnColor = visitor.getFlag("fs_add_to_cart_btn_color", "blue");
+
+  const fsEnableDiscountValue = fsEnableDiscount.getValue();
+  const fsAddToCartBtnColorValue = fsAddToCartBtnColor.getValue();
+
+  res.json({
+    item: {
+      name: "Flagship T-shirt",
+      price: 20,
+    },
+    fsEnableDiscount: fsEnableDiscountValue,
+    fsAddToCartBtnColor: fsAddToCartBtnColorValue,
+  });
 });
 
-/* end demo */
+app.post("/add-to-cart", async (req, res) => {
+  const visitor = await getVisitor();
+
+  // Step 5: Send a hit to track an action
+  visitor.sendHit({
+    type: HitType.EVENT,
+    category: EventCategory.ACTION_TRACKING,
+    action: "add-to-cart-clicked",
+  });
+
+  res.json(null);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+//end demo
