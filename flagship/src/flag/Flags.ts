fe5be7b089139737de6889c16ca17ac06b23a1f6
Heihokon <neoshark22@gmail.com>
2024-05-22 12:26:10 -0500

    Refactor Flags.ts for improved readability and documentation

--- flagship/src/flag/Flags.ts
+++ flagship/src/flag/Flags.ts
@@ -5,55 +5,18 @@ import { IFlagMetadata } from '../types'
 import { hasSameType, logDebugSprintf } from '../utils/utils'
 import { VisitorDelegate } from '../visitor/index'
 import { FlagMetadata } from './FlagMetadata'
+import { IFlag } from './IFlag'
 
-/**
- * This class represents a flag in the `Flagship SDK`. It helps you retrieve the flag value, access flag metadata,
- * expose the flag, verify the flag's existence, and get the flag status
- * @template T The type of the flag value.
- */
-export interface IFlag<T> {
-  /**
-   * Returns the value of the flag.
-   * If the flag exists and the type of the default value matches the flag type value.
-   * It can expose the flag if needed.
-   * @param visitorExposed Specifies whether to report the flag exposure. Default is true.
-   * @returns The value of the flag.
-   */
-  getValue(visitorExposed?: boolean): T;
-
-  /**
-   * Checks if the flag exists.
-   * @returns True if the flag exists, false otherwise.
-   */
-  exists: () => boolean;
-
-  /**
-   * Notifies Flagship that the visitor has been exposed to and seen this flag.
-   * @returns A promise that resolves when the notification is complete.
-   */
-  visitorExposed: () => Promise<void>;
-
-  /**
-   * Returns the metadata of the flag.
-   */
-  readonly metadata: IFlagMetadata;
-
-  /**
-   * Returns the status of the flag.
-   */
-  readonly status: FSFlagStatus;
-}
-
-export class Flag<T> implements IFlag<T> {
+export class Flag implements IFlag {
   private _visitor:VisitorDelegate
   private _key:string
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  private _defaultValue:any
-  constructor (param: {key:string, visitor:VisitorDelegate, defaultValue:T}) {
-    const { key, visitor, defaultValue } = param
+  private _defaultValue?:unknown
+
+  constructor (param: {key:string, visitor:VisitorDelegate}) {
+    const { key, visitor } = param
     this._key = key
     this._visitor = visitor
-    this._defaultValue = defaultValue
   }
 
   exists ():boolean {
@@ -92,13 +55,15 @@ export class Flag<T> implements IFlag<T> {
     return this._visitor.visitorExposed({ key: this._key, flag: flagDTO, defaultValue: this._defaultValue })
   }
 
-  getValue (userExposed = true) : T {
+  getValue <T> (defaultValue:T, visitorExposed = true) : T {
+    this._defaultValue = defaultValue
+
     const flagDTO = this._visitor.flagsData.get(this._key)
     return this._visitor.getFlagValue({
       key: this._key,
-      defaultValue: this._defaultValue,
+      defaultValue,
       flag: flagDTO,
-      userExposed
+      userExposed: visitorExposed
     })
   }
 
