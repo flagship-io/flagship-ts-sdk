65fdf79694ac2e6e8fe0d61f89bfb0cbfed7238a
Heihokon <neoshark22@gmail.com>
2024-07-09 08:49:09 -0500

    feat: Enable QA mode for Hit classes and DefaultStrategy

--- flagship/test/hit/Event.test.ts
+++ flagship/test/hit/Event.test.ts
@@ -11,6 +11,7 @@ import {
   EVENT_LABEL_API_ITEM,
   EVENT_VALUE_API_ITEM,
   HitType,
+  QA_MODE_API_ITEM,
   QT_API_ITEM,
   SDK_APP,
   TYPE_ERROR,
@@ -36,205 +37,241 @@ describe('test hit type Event', () => {
   afterAll(() => {
     Date.now = methodNow
   })
-  const category = EventCategory.ACTION_TRACKING
-  const action = 'action'
-  const event = new Event({ category, action, visitorId })
 
-  it('should ', () => {
-    expect(event.category).toBe(category)
-    expect(event.action).toBe(action)
-    expect(event.config).toBeUndefined()
-    expect(event.ds).toBe(SDK_APP)
-    expect(event.label).toBeUndefined()
-    expect(event.value).toBeUndefined()
-    expect(event.visitorId).toBe(visitorId)
-    expect(event.anonymousId).toBeNull()
-    expect(event.getErrorMessage()).toBe(ERROR_MESSAGE)
-    expect(event.userIp).toBeUndefined()
-    expect(event.screenResolution).toBeUndefined()
-    expect(event.locale).toBeUndefined()
-    expect(event.sessionNumber).toBeUndefined()
-  })
+  describe('test constructor ', () => {
+    it('test constructor with all params', () => {
+      const params = {
+        action: 'action',
+        category: EventCategory.ACTION_TRACKING,
+        label: 'label',
+        value: 12,
+        userIp: '127.0.0.1',
+        screenResolution: '800X600',
+        locale: 'fr',
+        sessionNumber: '12345',
+        visitorId,
+        qaMode: true
+      }
 
-  it('test constructor', () => {
-    const params = {
-      action: 'action',
-      category: EventCategory.ACTION_TRACKING,
-      label: 'label',
-      value: 12,
-      userIp: '127.0.0.1',
-      screenResolution: '800X600',
-      locale: 'fr',
-      sessionNumber: '12345',
-      visitorId
-    }
+      const event = new Event(params)
+      expect(event.category).toBe(params.category)
+      expect(event.action).toBe(params.action)
+      expect(event.label).toBe(params.label)
+      expect(event.value).toBe(params.value)
+      expect(event.userIp).toBe(params.userIp)
+      expect(event.screenResolution).toBe(params.screenResolution)
+      expect(event.locale).toBe(params.locale)
+      expect(event.sessionNumber).toBe(params.sessionNumber)
+      expect(event.qaMode).toBe(params.qaMode)
+    })
 
-    const event = new Event(params)
-    expect(event.category).toBe(params.category)
-    expect(event.action).toBe(params.action)
-    expect(event.label).toBe(params.label)
-    expect(event.value).toBe(params.value)
-    expect(event.userIp).toBe(params.userIp)
-    expect(event.screenResolution).toBe(params.screenResolution)
-    expect(event.locale).toBe(params.locale)
-    expect(event.sessionNumber).toBe(params.sessionNumber)
-  })
-
-  it('test isReady method ', () => {
-    expect(event.isReady()).toBeFalsy()
-  })
-
-  const logManager = new FlagshipLogManager()
-  const logError = jest.spyOn(logManager, 'error')
-  const config = new DecisionApiConfig({ envId: 'envId', apiKey: 'apiKey' })
-  config.logManager = logManager
-
-  it('test isReady method ', () => {
-    expect(event.isReady()).toBeFalsy()
-  })
-  it('test set config ', () => {
-    event.config = config
-    expect(event.config).toBe(config)
-  })
-
-  it('test set ds', () => {
-    event.ds = SDK_APP
-    expect(event.ds).toBe(SDK_APP)
-  })
-
-  it('test visitorId', () => {
-    event.visitorId = visitorId
-    expect(event.visitorId).toBe(visitorId)
-  })
-
-  const anonymousId = 'anonymousId'
-  it('test visitorId', () => {
-    event.anonymousId = anonymousId
-    expect(event.anonymousId).toBe(anonymousId)
-  })
-
-  it('test isReady method', () => {
-    expect(event.isReady()).toBeTruthy()
-    expect(event.isReady(false)).toBeTruthy()
-  })
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const apiKeys: Record<string, unknown> = {
-    [VISITOR_ID_API_ITEM]: anonymousId,
-    [DS_API_ITEM]: SDK_APP,
-    [CUSTOMER_ENV_ID_API_ITEM]: config.envId,
-    [T_API_ITEM]: HitType.EVENT,
-    [EVENT_CATEGORY_API_ITEM]: category,
-    [EVENT_ACTION_API_ITEM]: action,
-    [CUSTOMER_UID]: visitorId,
-    [QT_API_ITEM]: expect.anything()
-  }
-
-  it('should ', () => {
-    expect(event.toApiKeys()).toEqual(apiKeys)
-  })
-
-  it('test apiKey with anonymousId null', () => {
-    event.anonymousId = null
-    apiKeys[CUSTOMER_UID] = null
-    apiKeys[VISITOR_ID_API_ITEM] = visitorId
-    expect(event.toApiKeys()).toEqual(apiKeys)
-  })
-
-  const label = 'label'
-  // test label
-  it('test label ', () => {
-    event.label = label
-    expect(event.label).toBe(label)
-    apiKeys[EVENT_LABEL_API_ITEM] = label
-
-    expect(event.toApiKeys()).toEqual(apiKeys)
-
-    event.label = getNull()
-
-    expect(logError).toHaveBeenCalledWith(
-      sprintf(TYPE_ERROR, 'label', 'string'),
-      'label'
-    )
-    expect(event.label).toBe(label)
-    expect(logError).toHaveBeenCalledTimes(1)
-  })
-
-  const value = 122
-  // test set value
-  it('test set value', () => {
-    event.value = value
-    expect(event.value).toBe(value)
-    apiKeys[EVENT_VALUE_API_ITEM] = value
-
-    expect(event.toApiKeys()).toEqual(apiKeys)
-
-    event.value = {} as number
-    expect(logError).toBeCalledWith(
-      VALUE_FIELD_ERROR,
-      'value'
-    )
-    expect(event.value).toBe(value)
-
-    event.value = 2.5
-    expect(logError).toBeCalledWith(
-      VALUE_FIELD_ERROR,
-      'value'
-    )
-    expect(event.value).toBe(value)
-
-    event.value = -20
-    expect(logError).toBeCalledWith(
-      VALUE_FIELD_ERROR,
-      'value'
-    )
-    expect(event.value).toBe(value)
-    expect(logError).toHaveBeenCalledTimes(3)
-  })
-
-  it('test toObject', () => {
-    const userIp = '127.0.0.1'
-    const screenResolution = '800X600'
-    const locale = 'fr'
-    const sessionNumber = '12345'
-    const hitKey = 'key'
-    event.userIp = userIp
-    event.screenResolution = screenResolution
-    event.locale = locale
-    event.sessionNumber = sessionNumber
-    event.key = hitKey
-    expect(event.toObject()).toEqual({
-      action,
-      userIp,
-      screenResolution,
-      locale,
-      sessionNumber,
-      label,
-      key: hitKey,
-      createdAt: expect.anything(),
-      value,
-      anonymousId: null,
-      category,
-      ds: SDK_APP,
-      type: HitType.EVENT,
-      visitorId
+    it('test constructor with basic params', () => {
+      const category = EventCategory.ACTION_TRACKING
+      const action = 'action'
+      const event = new Event({ category, action, visitorId })
+      expect(event.category).toBe(category)
+      expect(event.action).toBe(action)
+      expect(event.config).toBeUndefined()
+      expect(event.ds).toBe(SDK_APP)
+      expect(event.label).toBeUndefined()
+      expect(event.value).toBeUndefined()
+      expect(event.visitorId).toBe(visitorId)
+      expect(event.anonymousId).toBeNull()
+      expect(event.getErrorMessage()).toBe(ERROR_MESSAGE)
+      expect(event.userIp).toBeUndefined()
+      expect(event.screenResolution).toBeUndefined()
+      expect(event.locale).toBeUndefined()
+      expect(event.sessionNumber).toBeUndefined()
+      expect(event.qaMode).toBeUndefined()
     })
   })
 
-  it('test log category', () => {
-    event.category = {} as EventCategory
-    expect(event.category).toBe(category)
-    expect(logError).toHaveBeenCalledTimes(1)
-    expect(logError).toBeCalledWith(CATEGORY_ERROR, 'category')
-  })
+  describe('test isReady method', () => {
+    const category = EventCategory.ACTION_TRACKING
+    const action = 'action'
+    const event = new Event({ category, action, visitorId })
+    event.qaMode = true
 
-  it('test log action ', () => {
-    event.action = ''
-    expect(logError).toHaveBeenCalledTimes(1)
-    expect(logError).toBeCalledWith(
-      sprintf(TYPE_ERROR, 'action', 'string'),
-      'action'
-    )
-    expect(event.action).toBe(action)
+    it('test isReady method ', () => {
+      expect(event.isReady()).toBeFalsy()
+    })
+
+    const logManager = new FlagshipLogManager()
+    const logError = jest.spyOn(logManager, 'error')
+    const config = new DecisionApiConfig({ envId: 'envId', apiKey: 'apiKey' })
+    config.logManager = logManager
+
+    it('test isReady method ', () => {
+      expect(event.isReady()).toBeFalsy()
+    })
+    it('test set config ', () => {
+      event.config = config
+      expect(event.config).toBe(config)
+    })
+
+    it('test set ds', () => {
+      event.ds = SDK_APP
+      expect(event.ds).toBe(SDK_APP)
+    })
+
+    it('test visitorId', () => {
+      event.visitorId = visitorId
+      expect(event.visitorId).toBe(visitorId)
+    })
+
+    const anonymousId = 'anonymousId'
+    it('test visitorId', () => {
+      event.anonymousId = anonymousId
+      expect(event.anonymousId).toBe(anonymousId)
+    })
+
+    it('test isReady method', () => {
+      expect(event.isReady()).toBeTruthy()
+      expect(event.isReady(false)).toBeTruthy()
+    })
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const apiKeys: Record<string, unknown> = {
+      [VISITOR_ID_API_ITEM]: anonymousId,
+      [DS_API_ITEM]: SDK_APP,
+      [CUSTOMER_ENV_ID_API_ITEM]: config.envId,
+      [T_API_ITEM]: HitType.EVENT,
+      [EVENT_CATEGORY_API_ITEM]: category,
+      [EVENT_ACTION_API_ITEM]: action,
+      [CUSTOMER_UID]: visitorId,
+      [QT_API_ITEM]: expect.anything(),
+      [QA_MODE_API_ITEM]: true
+    }
+
+    it('should ', () => {
+      expect(event.toApiKeys()).toEqual(apiKeys)
+    })
+
+    it('test apiKey with anonymousId null', () => {
+      event.anonymousId = null
+      apiKeys[CUSTOMER_UID] = null
+      apiKeys[VISITOR_ID_API_ITEM] = visitorId
+      expect(event.toApiKeys()).toEqual(apiKeys)
+    })
+
+    const label = 'label'
+    // test label
+    it('test label ', () => {
+      event.label = label
+      expect(event.label).toBe(label)
+      apiKeys[EVENT_LABEL_API_ITEM] = label
+
+      expect(event.toApiKeys()).toEqual(apiKeys)
+
+      event.label = getNull()
+
+      expect(logError).toHaveBeenCalledWith(
+        sprintf(TYPE_ERROR, 'label', 'string'),
+        'label'
+      )
+      expect(event.label).toBe(label)
+      expect(logError).toHaveBeenCalledTimes(1)
+    })
+
+    const value = 122
+    // test set value
+    it('test set value', () => {
+      event.value = value
+      expect(event.value).toBe(value)
+      apiKeys[EVENT_VALUE_API_ITEM] = value
+
+      expect(event.toApiKeys()).toEqual(apiKeys)
+
+      event.value = {} as number
+      expect(logError).toBeCalledWith(
+        VALUE_FIELD_ERROR,
+        'value'
+      )
+      expect(event.value).toBe(value)
+
+      event.value = 2.5
+      expect(logError).toBeCalledWith(
+        VALUE_FIELD_ERROR,
+        'value'
+      )
+      expect(event.value).toBe(value)
+
+      event.value = -20
+      expect(logError).toBeCalledWith(
+        VALUE_FIELD_ERROR,
+        'value'
+      )
+      expect(event.value).toBe(value)
+      expect(logError).toHaveBeenCalledTimes(3)
+    })
+
+    it('test toObject', () => {
+      const userIp = '127.0.0.1'
+      const screenResolution = '800X600'
+      const locale = 'fr'
+      const sessionNumber = '12345'
+      const hitKey = 'key'
+      event.userIp = userIp
+      event.screenResolution = screenResolution
+      event.locale = locale
+      event.sessionNumber = sessionNumber
+      event.key = hitKey
+
+      expect(event.toObject()).toEqual({
+        action,
+        userIp,
+        screenResolution,
+        locale,
+        sessionNumber,
+        label,
+        key: hitKey,
+        createdAt: expect.anything(),
+        value,
+        anonymousId: null,
+        category,
+        ds: SDK_APP,
+        type: HitType.EVENT,
+        visitorId,
+        qaMode: true
+      })
+    })
+
+    it('test log category', () => {
+      event.category = {} as EventCategory
+      expect(event.category).toBe(category)
+      expect(logError).toHaveBeenCalledTimes(1)
+      expect(logError).toBeCalledWith(CATEGORY_ERROR, 'category')
+    })
+
+    it('test log action ', () => {
+      event.action = ''
+      expect(logError).toHaveBeenCalledTimes(1)
+      expect(logError).toBeCalledWith(
+        sprintf(TYPE_ERROR, 'action', 'string'),
+        'action'
+      )
+      expect(event.action).toBe(action)
+    })
+
+    it('test log action ', () => {
+      const category = EventCategory.ACTION_TRACKING
+      const action = 'action'
+      const event2 = new Event({ category, action, visitorId })
+      event2.config = config
+      event2.visitorId = visitorId
+
+      const apiKeys: Record<string, unknown> = {
+        [VISITOR_ID_API_ITEM]: visitorId,
+        [DS_API_ITEM]: SDK_APP,
+        [CUSTOMER_ENV_ID_API_ITEM]: config.envId,
+        [T_API_ITEM]: HitType.EVENT,
+        [EVENT_CATEGORY_API_ITEM]: category,
+        [EVENT_ACTION_API_ITEM]: action,
+        [CUSTOMER_UID]: null,
+        [QT_API_ITEM]: expect.anything()
+      }
+
+      expect(event2.toApiKeys()).toEqual(apiKeys)
+    })
   })
 })
