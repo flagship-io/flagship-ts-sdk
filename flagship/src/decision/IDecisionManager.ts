536f3736ae6624a154de080fb81a0f3bd8500d73
Heihokon <neoshark22@gmail.com>
2024-10-24 15:43:05 -0500

    Refactor IDecisionManager to update getCampaignsAsync method signature

--- flagship/src/decision/IDecisionManager.ts
+++ flagship/src/decision/IDecisionManager.ts
@@ -1,25 +1,6 @@
-import { ITrackingManager } from '../api/ITrackingManager'
-import { IFlagshipConfig } from '../config/index'
-import { FSSdkStatus } from '../enum/index'
-import { CampaignDTO, FlagDTO, TroubleshootingData } from '../types'
+import { CampaignDTO } from '../types'
 import { VisitorAbstract } from '../visitor/VisitorAbstract'
 
 export interface IDecisionManager {
-  statusChangedCallback(func:(status: FSSdkStatus)=>void):void
-
-  isPanic(): boolean
-
-  getModifications (campaigns: Array<CampaignDTO>):Map<string, FlagDTO>
-
-  getCampaignsAsync (visitor: VisitorAbstract):Promise<CampaignDTO[]|null>
-
-  config:IFlagshipConfig
-
-  troubleshooting?: TroubleshootingData
-
-  lastBucketingTimestamp?: string
-
-  trackingManager : ITrackingManager
-  flagshipInstanceId : string
-
+  getCampaignsAsync(visitor: VisitorAbstract): Promise<CampaignDTO[]>;
 }
