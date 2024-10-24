bc36c0aac5e617cfd1fd782bad42eb7d93625192
Heihokon <neoshark22@gmail.com>
2024-10-24 15:42:30 -0500

    Refactor types.ts to add interfaces for Targetings, VariationGroupDTO, TroubleshootingDTO, and BucketingDTO

--- flagship/src/types.ts
+++ flagship/src/types.ts
@@ -293,3 +293,51 @@ export type SdkInfoType= {
   name: 'ReactJS'|'React-Native'|'Deno'|'TypeScript';
   version: string;
 }
+export interface Targetings{
+  operator:string,
+  key: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  value: any
+}
+export interface VariationGroupDTO {
+  id:string,
+  name?: string
+  targeting:{
+      targetingGroups:Array<{
+          targetings:Array<Targetings>
+      }>
+  },
+  variations:Array<{
+      id: string
+      name?: string
+      modifications: {
+          type: string
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          value:any
+      }
+      allocation?: number
+      reference?: boolean
+  }>
+}
+
+export type TroubleshootingDTO ={
+  startDate: string
+  endDate: string
+  traffic: number
+  timezone: string
+}
+
+export interface BucketingDTO {
+  panic?:boolean
+  campaigns?: Array<{
+      id: string
+      name?: string
+      type: string
+      slug?:string|null
+      variationGroups:Array<VariationGroupDTO>
+  }>
+  accountSettings?:{
+      enabledXPC?: true
+      troubleshooting?:TroubleshootingDTO
+  }
+}
