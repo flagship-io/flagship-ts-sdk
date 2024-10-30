60e1ac34b3d8606a6a52b721a7122801baa7d3e9
Heihokon <neoshark22@gmail.com>
2024-10-30 13:45:07 -0500

    refactor: Enhance ApiManager with detailed comments and improve error handling

--- flagship/src/visitor/ApiManager.ts
+++ flagship/src/visitor/ApiManager.ts
@@ -8,18 +8,45 @@ import { IBucketingPolling } from '../decision/IBucketingPolling'
 import { IDecisionManager } from './IDecisionManager'
 
 /**
- * This class is a decorator of DecisionManager to manage the visitor API
+ * It handles fetching campaigns for a visitor by making API calls.
  */
 export class ApiManager implements IDecisionManager {
+  /**
+   * Handles the polling mechanism for bucketing.
+   */
   protected bucketingPolling: IBucketingPolling
-  protected httpClient: IHttpClient
-  protected config: IFlagshipConfig
-  protected currentAbortController :AbortController|null
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  protected data:any
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  protected error:any
 
+  /**
+   * HTTP client used for making API requests.
+   */
+  protected httpClient: IHttpClient
+
+  /**
+   * Configuration object for Flagship settings.
+   */
+  protected config: IFlagshipConfig
+
+  /**
+   * Controller to manage the abortion of ongoing API requests.
+   */
+  protected currentAbortController: AbortController | null
+
+  /**
+   * Stores the fetched campaign data.
+   */
+  protected data: any
+
+  /**
+   * Stores any errors encountered during API requests.
+   */
+  protected error: any
+
+  /**
+   * Constructs an instance of ApiManager.
+   * @param bucketingPolling - The bucketing polling interface.
+   * @param httpClient - The HTTP client for making API requests.
+   * @param config - The Flagship configuration object.
+   */
   constructor (bucketingPolling: IBucketingPolling, httpClient: IHttpClient, config: IFlagshipConfig) {
     this.bucketingPolling = bucketingPolling
     this.httpClient = httpClient
@@ -29,12 +56,17 @@ export class ApiManager implements IDecisionManager {
     this.error = null
   }
 
+  /**
+   * Fetches campaign data for the specified visitor.
+   * @param visitor - The visitor for whom to fetch data.
+   */
   private async fetchData (visitor: VisitorAbstract) {
     if (this.currentAbortController) {
+      // Abort any existing request if a new one is initiated
       this.currentAbortController.abort('new-request')
     }
 
-    // Create a new AbortController for the current request
+    // Initialize a new AbortController for the current request
     this.currentAbortController = new AbortController()
 
     const headers = {
@@ -55,9 +87,11 @@ export class ApiManager implements IDecisionManager {
     const url = `${this.config.decisionApiUrl || BASE_API_URL}${this.config.envId}${URL_CAMPAIGNS}?${EXPOSE_ALL_KEYS}=true&extras[]=accountSettings`
     const now = Date.now()
     try {
+      // Reset data and error before making a new request
       this.data = null
       this.error = null
 
+      // Make the API call to fetch campaigns
       const response = await this.httpClient.postAsync(url, {
         headers,
         timeout: this.config.timeout,
@@ -66,25 +100,15 @@ export class ApiManager implements IDecisionManager {
         abortController: this.currentAbortController
       })
 
-      //   this.panic = !!response?.body?.panic
-
+      // Store the fetched data
       this.data = response?.body?.campaigns
-
-      //   const troubleshooting = response?.body?.extras?.accountSettings?.troubleshooting
-      //   if (troubleshooting) {
-      //     this.troubleshooting = {
-      //       startDate: new Date(troubleshooting.startDate),
-      //       endDate: new Date(troubleshooting.endDate),
-      //       timezone: troubleshooting.timezone,
-      //       traffic: troubleshooting.traffic
-      //     }
-      //   }
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error:any) {
+    } catch (error: any) {
       if (error === 'new-request') {
+        // Log if the request was aborted due to a new request
         logDebugSprintf(this.config, 'getCampaignsAsync', 'Request aborted')
         return
       }
+      // Format and store the error message
       const errorMessage = errorFormat(error.message || error, {
         url,
         headers,
@@ -93,19 +117,28 @@ export class ApiManager implements IDecisionManager {
       })
       this.error = new Error(errorMessage)
     } finally {
+      // Reset the AbortController after the request is completed
       this.currentAbortController = null
     }
   }
 
+  /**
+   * Asynchronously gets campaigns for the given visitor.
+   * @param visitor - The visitor for whom to get campaigns.
+   * @returns A promise that resolves to an array of CampaignDTO.
+   */
   public async getCampaignsAsync (visitor: VisitorAbstract): Promise<CampaignDTO[]> {
     return new Promise((resolve, reject) => {
+      // Initiate data fetching
       this.fetchData(visitor)
       const intervalId = setInterval(() => {
         if (this.data) {
+          // Resolve the promise when data is available
           resolve(this.data)
           clearInterval(intervalId)
         }
         if (this.error) {
+          // Reject the promise if an error occurred
           reject(this.error)
           clearInterval(intervalId)
         }
