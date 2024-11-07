c9cd558431f5b6978065afb7f19908773d985ce8
Heihokon <neoshark22@gmail.com>
2024-11-07 09:07:54 -0600

    Enhance EmotionAI class to process page views and manage click paths, integrating new event handling for improved visitor data collection

--- flagship/src/emotionAI/EmotionAI.ts
+++ flagship/src/emotionAI/EmotionAI.ts
@@ -1,6 +1,7 @@
 
-import { MAX_COLLECTING_TIME_MS, MAX_LAST_COLLECTING_TIME_MS } from '../enum/FlagshipConstant'
+import { MAX_CLICK_PATH_LENGTH, MAX_COLLECTING_TIME_MS, MAX_LAST_COLLECTING_TIME_MS, MAX_SCORING_POLLING_TIME } from '../enum/FlagshipConstant'
 import { CommonEmotionAI } from './CommonEmotionAI'
+import { PageView } from './hit/PageView'
 import { VisitorEvent } from './hit/VisitorEvent'
 
 export class EmotionAI extends CommonEmotionAI {
@@ -8,6 +9,12 @@ export class EmotionAI extends CommonEmotionAI {
   protected onMouseMove!: (event: MouseEvent) => void
   protected onMouseDown!: (event: MouseEvent) => void
   protected onMouseUp!: (event: MouseEvent) => void
+  protected _clickPath = ''
+  protected scrollTimeoutId: number | null = null
+  protected readonly scrollEndDelay: number = 200
+  protected _clickPathTimeoutId: number | null = null
+  protected _scoringInterval = 5000
+  protected _startScoringTimestamp!: number
 
   protected getCachedScore (cacheKey: string): string | null {
     if (typeof localStorage !== 'undefined') {
@@ -22,12 +29,39 @@ export class EmotionAI extends CommonEmotionAI {
     }
   }
 
-  protected startCollectingEAIData (visitorId: string): void {
+  protected async processPageView (visitorId: string): Promise<void> {
+    const pageView = new PageView({
+      visitorId,
+      customerAccountId: this._sdkConfig.envId as string,
+      currentUrl: window.location.href,
+      hasAdBlocker: false,
+      screenDepth: window.screen.colorDepth,
+      screenSize: `${window.innerWidth}x${window.innerHeight}`,
+      doNotTrack: navigator.doNotTrack || 'unspecified',
+      fonts: '',
+      hasFakeBrowserInfos: false,
+      hasFakeLanguageInfos: false,
+      hasFakeOsInfos: false,
+      hasFakeResolutionInfos: false,
+      userLanguage: navigator.language,
+      deviceCategory: 'desktop',
+      pixelRatio: window.devicePixelRatio,
+      documentReferer: document.referrer,
+      viewportSize: `${document.documentElement.clientWidth}x${document.documentElement.clientHeight}`,
+      touchSupport: 'ontouchstart' in window ? 'yes' : 'no',
+      userAgent: navigator.userAgent
+    })
+
+    await this.sendPageView(pageView)
+  }
+
+  protected async startCollectingEAIData (visitorId: string): Promise<void> {
+    await this.processPageView(visitorId)
     this._isEAIDataCollecting = true
     this._startCollectingEAIDataTimestamp = Date.now()
     let mouseDownTimestamp: number | null = null
 
-    this.onScroll = (event: Event) => this.handleScroll(event, visitorId)
+    this.onScroll = () => this.handleScroll(visitorId)
     this.onMouseMove = (event: MouseEvent) => this.handleMouseMove(event, visitorId)
 
     this.onMouseDown = () => {
@@ -47,34 +81,111 @@ export class EmotionAI extends CommonEmotionAI {
     document.addEventListener('mouseup', this.onMouseUp)
   }
 
-  protected stopCollectingEAIData (): void {
+  protected removeListeners (): void {
     this._isEAIDataCollecting = false
     window.removeEventListener('scroll', this.onScroll)
     document.removeEventListener('mousemove', this.onMouseMove)
     document.removeEventListener('mousedown', this.onMouseDown)
     document.removeEventListener('mouseup', this.onMouseUp)
+    if (this.scrollTimeoutId !== null) {
+      clearTimeout(this.scrollTimeoutId)
+      this.scrollTimeoutId = null
+    }
+    if (this._clickPathTimeoutId !== null) {
+      clearTimeout(this._clickPathTimeoutId)
+      this._clickPathTimeoutId = null
+    }
   }
 
-  private handleScroll = (event: Event, visitorId: string): void => {
-    console.log('scroll', event, visitorId)
+  protected stopCollectingEAIData (visitorId:string): void {
+    this.removeListeners()
+    this._startScoringTimestamp = Date.now()
+
+    const scoringIntervalId = setInterval(async () => {
+      if (Date.now() - this._startScoringTimestamp > MAX_SCORING_POLLING_TIME) {
+        clearInterval(scoringIntervalId)
+      }
+      this._EAIScoreChecked = false
+      const score = await this.fetchEAIScore(visitorId)
+      if (score) {
+        clearInterval(scoringIntervalId)
+      }
+    }, this._scoringInterval)
   }
 
-  private handleMouseMove = (event: MouseEvent, visitorId: string): void => {
-    console.log('mousemove', event, visitorId)
+  protected onScrollEnd (visitorId: string): void {
+    const timestamp = Date.now().toString().slice(-5)
+    const scrollPosition = `${window.scrollY},${timestamp};`
+    const visitorEvent = new VisitorEvent({
+      visitorId,
+      customerAccountId: this._sdkConfig.envId as string,
+      scrollPosition,
+      screenSize: `${window.innerWidth},${window.innerHeight};`,
+      currentUrl: window.location.href
+    })
+    this.processVisitorEvent(visitorEvent, visitorId)
   }
 
-  private handleClick = (event: MouseEvent, visitorId: string, clickDuration: number): void => {
+  protected handleScroll = (visitorId: string): void => {
+    if (this.scrollTimeoutId !== null) {
+      clearTimeout(this.scrollTimeoutId)
+    }
+
+    this.scrollTimeoutId = window.setTimeout(() => {
+      this.onScrollEnd(visitorId)
+    }, this.scrollEndDelay)
+  }
+
+  protected processVisitorEvent (visitorEvent: VisitorEvent, visitorId: string): void {
+    const timestampDiff = Date.now() - this._startCollectingEAIDataTimestamp
+    if (timestampDiff <= MAX_COLLECTING_TIME_MS) {
+      this.sendVisitorEvent(visitorEvent)
+    }
+
+    if ((timestampDiff > MAX_COLLECTING_TIME_MS && timestampDiff <= MAX_LAST_COLLECTING_TIME_MS)) {
+      this.sendVisitorEvent(visitorEvent)
+      this.stopCollectingEAIData(visitorId)
+    }
+    if (timestampDiff > MAX_LAST_COLLECTING_TIME_MS) {
+      this.removeListeners()
+    }
+  }
+
+  protected sendClickPath (visitorId: string): void {
+    const visitorEvent = new VisitorEvent({
+      visitorId,
+      customerAccountId: this._sdkConfig.envId as string,
+      clickPath: this._clickPath,
+      screenSize: `${window.innerWidth},${window.innerHeight};`,
+      currentUrl: window.location.href
+    })
+    this._clickPath = ''
+    this.processVisitorEvent(visitorEvent, visitorId)
+  }
+
+  protected handleMouseMove = (event: MouseEvent, visitorId: string): void => {
+    if (this._clickPathTimeoutId !== null) {
+      clearTimeout(this._clickPathTimeoutId)
+    }
+    this._clickPath += `${event.clientX},${event.clientY},${Date.now().toString().slice(-5)};`
+    if (this._clickPath.length >= MAX_CLICK_PATH_LENGTH) {
+      this.sendClickPath(visitorId)
+      return
+    }
+    this._clickPathTimeoutId = window.setTimeout(() => {
+      this.sendClickPath(visitorId)
+    }, 500)
+  }
+
+  protected handleClick = (event: MouseEvent, visitorId: string, clickDuration: number): void => {
     const timestamp = Date.now().toString().slice(-5)
     const visitorEvent = new VisitorEvent({
       visitorId,
       customerAccountId: this._sdkConfig.envId as string,
       clickPosition: `${event.clientX},${event.clientY},${timestamp},${clickDuration};`,
-      screenSize: `${window.innerWidth},${window.innerHeight}`,
+      screenSize: `${window.innerWidth},${window.innerHeight};`,
       currentUrl: window.location.href
     })
-    const timestampDiff = Date.now() - this._startCollectingEAIDataTimestamp
-    if (timestampDiff >= MAX_COLLECTING_TIME_MS && timestampDiff <= MAX_LAST_COLLECTING_TIME_MS) {
-      this.stopCollectingEAIData()
-    }
+    this.processVisitorEvent(visitorEvent, visitorId)
   }
 }
