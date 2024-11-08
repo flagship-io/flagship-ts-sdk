7574f30b2a8b6987aef1f88dc9950ee41d53dd30
Heihokon <neoshark22@gmail.com>
2024-11-08 13:39:08 -0600

    Add ApiSdkManager class implementing ISdkManager for API management

---
+++ flagship/src/main/ApiSdkManager.ts
@@ -0,0 +1,49 @@
+import { EAIConfig } from '../type.local'
+import { BucketingDTO } from '../types'
+import { ISdkManager } from './ISdkManager'
+import { ITrackingManager } from '../api/ITrackingManager'
+import { IHttpClient } from '../utils/HttpClient'
+import { IFlagshipConfig } from '../config/IFlagshipConfig'
+import { logErrorSprintf, sprintf } from 'src/utils/utils'
+import { CDN_ACCOUNT_SETTINGS_URL } from '../enum/FlagshipConstant'
+
+type constructorParam = {
+  httpClient: IHttpClient;
+  sdkConfig: IFlagshipConfig;
+  trackingManager: ITrackingManager;
+}
+
+export class ApiSdkManager implements ISdkManager {
+  protected _httpClient: IHttpClient
+  protected _config: IFlagshipConfig
+  protected _trackingManager: ITrackingManager
+  protected _EAIConfig?: EAIConfig
+
+  public constructor ({ httpClient, sdkConfig, trackingManager }: constructorParam) {
+    this._httpClient = httpClient
+    this._config = sdkConfig
+    this._trackingManager = trackingManager
+  }
+
+  resetSdk (): void {
+    this._EAIConfig = undefined
+  }
+
+  getBucketingContent (): BucketingDTO | undefined {
+    return undefined
+  }
+
+  async initSdk (): Promise<void> {
+    try {
+      const url = sprintf(CDN_ACCOUNT_SETTINGS_URL, this._config.envId)
+      const response = await this._httpClient.getAsync(url)
+      this._EAIConfig = response.body
+    } catch (error:any) {
+      logErrorSprintf(this._config, 'Error while fetching EAI config: {0}', error?.message || error)
+    }
+  }
+
+  getEAIConfig (): EAIConfig|undefined {
+    return this._EAIConfig as EAIConfig
+  }
+}
