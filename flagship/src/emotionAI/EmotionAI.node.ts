4ea468f0b33040cafc41d62d2994a3cf6b97f913
Heihokon <neoshark22@gmail.com>
2024-11-18 19:15:32 -0600

    Add reportVisitorEvent method to EmotionAI class

--- flagship/src/emotionAI/EmotionAI.node.ts
+++ flagship/src/emotionAI/EmotionAI.node.ts
@@ -1,7 +1,11 @@
-
 import { CommonEmotionAI } from './CommonEmotionAI'
+import { IVisitorEvent } from './hit/IVisitorEvent'
 
 export class EmotionAI extends CommonEmotionAI {
+  public async reportVisitorEvent (visitorEvent: IVisitorEvent): Promise<void> {
+    //
+  }
+
   public cleanup (): void {
     //
   }
