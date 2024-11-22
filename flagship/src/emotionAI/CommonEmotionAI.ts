d0f3eb5903d925aea2a82b917b2b6b371f98e197
Heihokon <neoshark22@gmail.com>
2024-11-22 16:20:04 -0600

    Refactor CommonEmotionAI: streamline visitor handling and improve score fetching logic

--- flagship/src/emotionAI/CommonEmotionAI.ts
+++ flagship/src/emotionAI/CommonEmotionAI.ts
@@ -1,11 +1,13 @@
 import { IHttpClient, IHttpResponse } from '../utils/HttpClient'
 import { IEmotionAI } from './IEmotionAI'
-import { EAIConfig, EAIScore } from '../type.local'
+import { EAIConfig } from '../type.local'
 import { logDebugSprintf, logErrorSprintf, sprintf } from '../utils/utils'
-import { EMOTION_AI_EVENT_URL, EMOTION_AI_UC_URL, FETCH_EAI_SCORE, FETCH_EAI_SCORE_ERROR, FETCH_EAI_SCORE_SUCCESS, HEADER_APPLICATION_JSON, HEADER_CONTENT_TYPE, SEND_EAI_EVENT, SEND_EAI_EVENT_ERROR, SEND_EAI_EVENT_SUCCESS, VISITOR_EAI_SCORE_KEY } from '../enum/FlagshipConstant'
+import { EMOTION_AI_EVENT_URL, EMOTION_AI_UC_URL, FETCH_EAI_SCORE, FETCH_EAI_SCORE_ERROR, FETCH_EAI_SCORE_SUCCESS, HEADER_APPLICATION_JSON, HEADER_CONTENT_TYPE, SEND_EAI_EVENT, SEND_EAI_EVENT_ERROR, SEND_EAI_EVENT_SUCCESS } from '../enum/FlagshipConstant'
 import { IVisitorEvent } from './hit/IVisitorEvent'
 import { IPageView } from './hit/IPageView'
 import { IFlagshipConfig } from '../config/IFlagshipConfig'
+import { EAIScore } from '../types'
+import { VisitorAbstract } from '../visitor/VisitorAbstract'
 
 type ConstructorParam = {
   httpClient: IHttpClient;
@@ -25,6 +27,7 @@ export abstract class CommonEmotionAI implements IEmotionAI {
   protected _scoringIntervalId?: NodeJS.Timeout
   protected _scoringInterval = 5000
   protected _onEAICollectStatusChange?: (status: boolean) => void
+  protected _visitor!: VisitorAbstract
   /**
    * Indicates whether EAI data has been collected
    */
@@ -40,6 +43,10 @@ export abstract class CommonEmotionAI implements IEmotionAI {
     this._isEAIDataCollected = false
   }
 
+  public init (visitor: VisitorAbstract): void {
+    this._visitor = visitor
+  }
+
   onEAICollectStatusChange (callback: (status: boolean) => void): void {
     this._onEAICollectStatusChange = callback
   }
@@ -52,13 +59,10 @@ export abstract class CommonEmotionAI implements IEmotionAI {
     return this._EAIScoreChecked
   }
 
-  protected abstract getCachedScore (cacheKey: string): Promise<string | null> ;
-
-  protected abstract setCachedScore (cacheKey: string, score: string): Promise<void> ;
-
   public abstract cleanup (): void ;
 
-  public async fetchEAIScore (visitorId:string): Promise<EAIScore|undefined> {
+  public async fetchEAIScore (): Promise<EAIScore|undefined> {
+    const visitorId = this._visitor.visitorId
     if (this._fetchEAIScorePromise) {
       await this._fetchEAIScorePromise
       return this._EAIScore
@@ -72,11 +76,10 @@ export abstract class CommonEmotionAI implements IEmotionAI {
       return this._EAIScore
     }
 
-    const cacheKey = sprintf(VISITOR_EAI_SCORE_KEY, visitorId)
-    const cachedEAIScore = await this.getCachedScore(cacheKey)
+    const cachedEAIScore = this._visitor.getCachedEAIScore()
 
     if (cachedEAIScore) {
-      this._EAIScore = JSON.parse(cachedEAIScore)
+      this._EAIScore = cachedEAIScore
       this._EAIScoreChecked = true
       return this._EAIScore
     }
@@ -87,8 +90,9 @@ export abstract class CommonEmotionAI implements IEmotionAI {
       const response = await this._fetchEAIScorePromise
       this._EAIScore = response.body
       this._EAIScoreChecked = true
+
       if (this._EAIScore) {
-        this.setCachedScore(cacheKey, JSON.stringify(this._EAIScore))
+        this._visitor.setCachedEAIScore(this._EAIScore)
       }
 
       logDebugSprintf(this._sdkConfig, FETCH_EAI_SCORE, FETCH_EAI_SCORE_SUCCESS, visitorId, this._EAIScore)
@@ -109,15 +113,16 @@ export abstract class CommonEmotionAI implements IEmotionAI {
 
   protected abstract startCollectingEAIData (visitorId: string, currentPage?: Omit<IPageView, 'toApiKeys'>): Promise<void> ;
 
-  public async collectEAIData (visitorId: string, currentPage?: Omit<IPageView, 'toApiKeys'>): Promise<void> {
+  public async collectEAIData (currentPage?: Omit<IPageView, 'toApiKeys'>): Promise<void> {
     if (!this._eAIConfig?.eaiActivationEnabled || this._isEAIDataCollecting ||
       !this._eAIConfig?.eaiCollectEnabled || this._isEAIDataCollected) {
       return
     }
-    const score = await this.fetchEAIScore(visitorId)
+    const score = await this.fetchEAIScore()
     if (score) {
       return
     }
+    const visitorId = this._visitor.visitorId
     await this.startCollectingEAIData(visitorId, currentPage)
   }
 
