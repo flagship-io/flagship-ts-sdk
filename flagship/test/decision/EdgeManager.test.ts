171c562c9913033784599bb1885d0827dd9da569
Heihokon <neoshark22@gmail.com>
2024-11-27 16:03:37 -0600

    Integrate emotionAi into EdgeManager tests: enhance VisitorDelegate initialization and mock bucketing content

--- flagship/test/decision/EdgeManager.test.ts
+++ flagship/test/decision/EdgeManager.test.ts
@@ -1,35 +1,55 @@
 import { IDecisionManager } from './../../src/decision/IDecisionManager'
 import { EdgeConfig } from './../../src/config/EdgeConfig'
 import { EdgeManager } from './../../src/decision/EdgeManager'
-import { it, describe } from '@jest/globals'
+import { it, describe, jest } from '@jest/globals'
 import { VisitorDelegate } from '../../src/visitor/VisitorDelegate'
 import { HttpClient } from '../../src/utils/HttpClient'
 import { MurmurHash } from '../../src/utils/MurmurHash'
 import { TrackingManager } from '../../src/api/TrackingManager'
-
-// const getCampaignsAsync: Mock<Promise<CampaignDTO[] | null>, [VisitorAbstract]> = jest.fn()
+import { ISdkManager } from '../../src/main/ISdkManager'
+import { BucketingDTO, CampaignDTO } from '../../src/types'
+import { VisitorAbstract } from '../../src/visitor/VisitorAbstract'
+import { IEmotionAI } from '../../src/emotionAI/IEmotionAI'
+import { bucketing } from './bucketing'
 
 describe('Test EdgeManager', () => {
   const murmurHash = new MurmurHash()
   const httpClient = new HttpClient()
   const config = new EdgeConfig()
-  const edgeManager = new EdgeManager(httpClient, config, murmurHash)
+
+  const getBucketingContent = jest.fn<() => BucketingDTO | undefined>()
+
+  const sdkManager = {
+    getBucketingContent
+  } as unknown as ISdkManager
+
+  getBucketingContent.mockReturnValue(undefined)
+
+  const edgeManager = new EdgeManager({ httpClient, config, murmurHash, sdkManager })
+
+  const emotionAi = {
+    init: jest.fn<(visitor:VisitorAbstract) => void>()
+
+  } as unknown as IEmotionAI
 
   const visitor = new VisitorDelegate({
     hasConsented: true,
-    visitorId: 'visitorId',
-    context: {},
+    visitorId: 'visitor_1',
+    context: {
+      age: 20
+    },
     configManager: {
       config,
       decisionManager: {} as IDecisionManager,
       trackingManager: new TrackingManager(httpClient, config)
-    }
+    },
+    emotionAi
   })
   it('test getCampaign', async () => {
+    getBucketingContent.mockReturnValue(bucketing)
     // getCampaignsAsync.mockResolvedValue(null)
-    await edgeManager.getCampaignsAsync(visitor)
-
-    // expect(getCampaignsAsync).toBeCalledTimes(1)
-    // expect(getCampaignsAsync).toBeCalledWith(visitor)
+    const campaigns = await edgeManager.getCampaignsAsync(visitor)
+    const modifications = edgeManager.getModifications(campaigns as CampaignDTO[])
+    expect(modifications.size).toBe(6)
   })
 })
