8b13c14c5cc30b1a396d1a54df6154804eacf5f1
Heihokon <neoshark22@gmail.com>
2024-12-03 15:02:34 -0600

    Rename collectEAIData method to collectEAIDataAsync and update return type to Promise<void>

--- flagship/src/visitor/NoConsentStrategy.ts
+++ flagship/src/visitor/NoConsentStrategy.ts
@@ -23,7 +23,7 @@ export class NoConsentStrategy extends DefaultStrategy {
     //
   }
 
-  public collectEAIData (): void {
+  public async collectEAIDataAsync (): Promise<void> {
     this.log('collectEAIData')
   }
 
