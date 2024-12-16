10c2419623f9cc2f52205fb6d31a0f129fa28744
Heihokon <neoshark22@gmail.com>
2024-12-16 14:18:47 -0600

    Fix typo in initTroubleshootingHit property name in BatchingCachingStrategyConstruct type

--- flagship/src/api/types.ts
+++ flagship/src/api/types.ts
@@ -14,7 +14,7 @@ export type BatchingCachingStrategyConstruct = {
     troubleshootingQueue: Map<string, Troubleshooting>
     analyticHitQueue: Map<string, UsageHit>
     flagshipInstanceId?:string
-    initTroubleshootingHi?: Troubleshooting
+    initTroubleshootingHit?: Troubleshooting
    }
 
 export type SendActivate = {
