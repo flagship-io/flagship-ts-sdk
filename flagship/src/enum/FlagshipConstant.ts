33eec7df17b1bc97abb51a3377866f8a4bd29a69
Heihokon <neoshark22@gmail.com>
2025-01-21 13:59:21 -0600

    Remove partner query parameter from EMOTION_AI_UC_URL for cleaner URL structure

--- flagship/src/enum/FlagshipConstant.ts
+++ flagship/src/enum/FlagshipConstant.ts
@@ -74,7 +74,7 @@ export const FS_IS_QA_MODE_ENABLED = 'FS_IS_QA_MODE_ENABLED'
 export const FS_FORCED_VARIATIONS = 'FS_FORCED_VARIATIONS'
 export const FS_QA_ASSISTANT_SCRIPT_TAG_ID = 'FS_QA_ASSISTANT_SCRIPT_TAG_ID'
 
-export const EMOTION_AI_UC_URL = 'https://uc-info.flagship.io/v1/segments/clients/{0}/visitors/{1}?partner=eai'
+export const EMOTION_AI_UC_URL = 'https://uc-info.flagship.io/v1/segments/clients/{0}/visitors/{1}'
 export const CDN_ACCOUNT_SETTINGS_URL = 'https://cdn.flagship.io/{0}/accountSettings.json'
 export const VISITOR_EAI_SCORE_KEY = '{0}_EAIScore'
 export const EMOTION_AI_EVENT_URL = 'https://events.flagship.io/emotionsai'
