699c215249e800fb2d2ac052128f631b1d877da0
Heihokon <neoshark22@gmail.com>
2025-02-28 09:33:24 -0600

    Add VisitorProfileCache class for managing visitor profiles in Node.js

--- flagship/src/visitor/VisitorProfileCache.node.ts
+++ flagship/src/visitor/VisitorProfileCache.node.ts
@@ -2,7 +2,7 @@ import { IFlagshipConfig } from '../config/IFlagshipConfig'
 import { IVisitorProfileCache } from '../type.local'
 import { VisitorProfile } from '../types'
 
-export class VisitorProfileCacheNode implements IVisitorProfileCache {
+export class VisitorProfileCache implements IVisitorProfileCache {
   private _sdkConfig:IFlagshipConfig
   constructor (sdkConfig:IFlagshipConfig) {
     this._sdkConfig = sdkConfig
