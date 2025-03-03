d567df2c0aa4c9dc3873b27d841a04cb26190aac
Heihokon <neoshark22@gmail.com>
2025-03-03 14:44:28 -0600

    Update package.json and webpack configuration to include source-map-explorer and upgrade webpack-bundle-analyzer

--- flagship/webpack/webpack.common.cjs
+++ flagship/webpack/webpack.common.cjs
@@ -3,6 +3,9 @@ const path = require('path')
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+
 const isProduction = process.env.NODE_ENV === 'production'
 
 const config = {
@@ -26,6 +29,10 @@ module.exports = () => {
     config.mode = 'production'
   } else {
     config.mode = 'development'
+    config.plugins = [new BundleAnalyzerPlugin({
+      analyzerMode: 'static'
+    })]
   }
+
   return config
 }
