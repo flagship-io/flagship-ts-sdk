26da25680ade12804815cebf644612fa5e7a5a6e
Heihokon <neoshark22@gmail.com>
2025-03-28 12:35:05 -0500

    Add return type to myFetch function and update build scripts for improved clarity

--- flagship/src/depsNode.native.ts
+++ flagship/src/depsNode.native.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const myFetch = (input: RequestInfo | URL, init?: RequestInit) => fetch(input, init)
+export const myFetch = (input: RequestInfo | URL, init?: RequestInit):Promise<Response> => fetch(input, init)
 export { EventEmitter } from 'events'
 export const LocalAbortController = AbortController
