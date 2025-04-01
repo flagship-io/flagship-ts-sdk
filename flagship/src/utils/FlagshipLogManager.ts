1831e06b0c8bb988808eecdaea47e739e6805e99
Heihokon <neoshark22@gmail.com>
2025-04-01 15:08:39 -0500

    Refactor FlagshipLogManager to use formatLogOutput for logging methods

--- flagship/src/utils/FlagshipLogManager.ts
+++ flagship/src/utils/FlagshipLogManager.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
-import { FLAGSHIP_SDK, LogLevel } from '../enum/index'
+import {  LogLevel } from '../enum/index';
+import { formatLogOutput } from './utils';
 
 export interface IFlagshipLogManager {
   /**
@@ -20,93 +21,52 @@ export interface IFlagshipLogManager {
 
 export class FlagshipLogManager implements IFlagshipLogManager {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  consoleError: { (...data: any[]): void; (message?: any, ...optionalParams: any[]): void; }
+  consoleError: { (...data: any[]): void; (message?: any, ...optionalParams: any[]): void; };
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  consoleWarn: { (...data: any[]): void; (message?: any, ...optionalParams: any[]): void; }
+  consoleWarn: { (...data: any[]): void; (message?: any, ...optionalParams: any[]): void; };
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  consoleDebug: { (...data: any[]): void; (message?: any, ...optionalParams: any[]): void; }
+  consoleDebug: { (...data: any[]): void; (message?: any, ...optionalParams: any[]): void; };
 
-  constructor () {
-    this.consoleError = console.error ?? console.log
-    this.consoleWarn = console.warn ?? console.log
-    this.consoleDebug = console.debug ?? console.log
+  constructor() {
+    this.consoleError = console.error ?? console.log;
+    this.consoleWarn = console.warn ?? console.log;
+    this.consoleDebug = console.debug ?? console.log;
   }
 
-  emergency (message: string, tag: string): void {
-    this.consoleError(this.formatOutput(LogLevel.EMERGENCY, message, tag))
+  emergency(message: string, tag: string): void {
+    this.consoleError(formatLogOutput(LogLevel.EMERGENCY, message, tag));
   }
 
-  alert (message: string, tag: string): void {
-    this.consoleError(this.formatOutput(LogLevel.ALERT, message, tag))
+  alert(message: string, tag: string): void {
+    this.consoleError(formatLogOutput(LogLevel.ALERT, message, tag));
   }
 
-  critical (message: string, tag: string): void {
-    this.consoleError(this.formatOutput(LogLevel.CRITICAL, message, tag))
+  critical(message: string, tag: string): void {
+    this.consoleError(formatLogOutput(LogLevel.CRITICAL, message, tag));
   }
 
-  error (message: string, tag: string): void {
-    this.consoleError(this.formatOutput(LogLevel.ERROR, message, tag))
+  error(message: string, tag: string): void {
+    this.consoleError(formatLogOutput(LogLevel.ERROR, message, tag));
   }
 
-  warning (message: string, tag: string): void {
-    this.consoleWarn(this.formatOutput(LogLevel.WARNING, message, tag))
+  warning(message: string, tag: string): void {
+    this.consoleWarn(formatLogOutput(LogLevel.WARNING, message, tag));
   }
 
-  notice (message: string, tag: string): void {
-    this.log(LogLevel.NOTICE, message, tag)
+  notice(message: string, tag: string): void {
+    this.log(LogLevel.NOTICE, message, tag);
   }
 
-  info (message: string, tag: string): void {
-    console.info(this.formatOutput(LogLevel.INFO, message, tag))
+  info(message: string, tag: string): void {
+    console.info(formatLogOutput(LogLevel.INFO, message, tag));
   }
 
-  debug (message: string, tag: string): void {
-    this.consoleDebug(this.formatOutput(LogLevel.DEBUG, message, tag))
+  debug(message: string, tag: string): void {
+    this.consoleDebug(formatLogOutput(LogLevel.DEBUG, message, tag));
   }
 
-  log (level: LogLevel, message: string, tag: string): void {
-    console.log(this.formatOutput(level, message, tag))
+  log(level: LogLevel, message: string, tag: string): void {
+    console.log(formatLogOutput(level, message, tag));
   }
 
-  protected formatOutput(level: LogLevel, message: string, tag: string): string {
-    const now = new Date()
-    
-    const formatTwoDigits = (value: number): string => {
-      return value.toString().padStart(2, '0')
-    }
-    
-    const formatMilliseconds = (value: number): string => {
-      return value.toString().padStart(3, '0')
-    }
-    
-    const colorCodes: Record<LogLevel, string> = {
-      [LogLevel.EMERGENCY]: '\x1b[1;37;41m',
-      [LogLevel.ALERT]: '\x1b[1;37;45m',
-      [LogLevel.CRITICAL]: '\x1b[1;37;41m',
-      [LogLevel.ERROR]: '\x1b[1;37;41m',
-      [LogLevel.WARNING]: '\x1b[33;1m',
-      [LogLevel.NOTICE]: '\x1b[36;1m',
-      [LogLevel.INFO]: '\x1b[32;1m',
-      [LogLevel.DEBUG]: '\x1b[90;1m',
-      [LogLevel.NONE]: '',
-      [LogLevel.ALL]: '\x1b[90;1m',
-    }
-  
-    const resetColor = '\x1b[0m'
-    const colorCode = colorCodes[level] || ''
-  
-    const year = now.getFullYear()
-    const month = formatTwoDigits(now.getMonth() + 1)
-    const day = formatTwoDigits(now.getDate())
-    const hours = formatTwoDigits(now.getHours())
-    const minutes = formatTwoDigits(now.getMinutes())
-    const seconds = formatTwoDigits(now.getSeconds())
-    const milliseconds = formatMilliseconds(now.getMilliseconds())
-    
-    const timestamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`
-    
-    const levelName = LogLevel[level].padEnd(2)
-    
-    return `${colorCode}[${timestamp}] [${FLAGSHIP_SDK}] [${levelName}] [${tag}] ${message}${resetColor}`
-  }
 }
